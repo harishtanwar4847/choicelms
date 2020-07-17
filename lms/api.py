09cd46e746f842028011fd5f3a53a6880d892f90
Neel Bhanushali <neal.bhanushali@atritechnocrat.in>
2020-07-17 15:08:51 +0530

    user kyc done

--- lms/api.py
+++ lms/api.py
@@ -28,6 +28,9 @@ import requests
 from frappe.core.doctype.sms_settings.sms_settings import send_sms
 from frappe.auth import LoginManager
 from frappe.utils.password import delete_login_failed_cache
+from datetime import datetime
+from xmljson import parker
+from xml.etree.ElementTree import fromstring
 
 
 def appErrorLog(title, error):
@@ -357,6 +360,104 @@ def get_user(input):
 		return user_data[0].name
 	else:
 		return False
+
+@frappe.whitelist()
+def get_user_kyc(pan_no, birth_date):
+	try:
+		if not pan_no:
+			return generateResponse(status=422, message="Pan is required.")
+		if not birth_date:
+			return generateResponse(status=422, message="Birth date is required.")
+
+		datetime.strptime(birth_date, "%d/%m/%Y")
+
+		user_kyc_list = frappe.db.get_all("User KYC", filters={ "user": frappe.session.user }, order_by="user_type", fields=["*"])
+
+		if len(user_kyc_list) > 0:
+			pass
+			# return generateResponse(message="User KYC", data=user_kyc_list[0])
+
+		# check in choice
+		url = "https://uat-pwa.choicetechlab.com/api/spark/getByPanNumDetails"
+		params = {"panNum": pan_no}
+		headers = {
+			"businessUnit": "JF",
+			"userId": "Spark",
+			"investorId": "1",
+			"ticket": "c3Bhcms="
+		}
+		r = requests.get(url, params=params, headers=headers)
+		data = json.loads(r.text)
+		if 'status' not in data:
+			user_kyc = frappe.get_doc({
+				"doctype": "User KYC",
+				"user": frappe.session.user,
+				"user_type": "CHOICE",
+				"investor_name": data["investorName"],
+				"father_name": data["fatherName"],
+				"mother_name": data["motherName"],
+				"address": data["address"],
+				"city": data["addressCity"],
+				"state": data["addressState"],
+				"pincode": data["addressPinCode"],
+				"mobile_number": data["mobileNum"],
+				"choice_client_id": data["clientId"],
+				"pan_no": data["panNum"],
+				"aadhar_no": data["panNum"],
+				"bank_account_type": data["accountType"],
+				"bank_name": data["bank"],
+				"bank_code": data["bankcode"],
+				"bank_mode": data["bankMode"],
+				"bank_branch": data["branch"],
+				"bank_ifsc": data["ifsc"],
+				"bank_micr": data["micr"],
+				"bank_account_number": data["accountNumber"],
+				"bank_address": data["bankAddress"],
+				"bank_address_district": data["district"],
+				"bank_address_city": data["city"],
+				"bank_address_state": data["state"],
+				"bank_address_pincode": data["bankZipCode"],
+				"bank_contact": data["contact"]
+			})
+			user_kyc.insert(ignore_permissions=True)
+			frappe.db.commit()
+			return generateResponse(message="CHOICE USER KYC", data={"user_kyc": user_kyc, "response": data})
+
+		# check in kra
+		url = "https://www.cvlkra.com/paninquiry.asmx/SolicitPANDetailsFetchALLKRA"
+		params = {
+			"inputXML": "<APP_REQ_ROOT><APP_PAN_INQ> <APP_PAN_NO>{0}</APP_PAN_NO> <APP_DOB_INCORP>{1}</APP_DOB_INCORP> <APP_POS_CODE>1100066900</APP_POS_CODE> <APP_RTA_CODE>1100066900</APP_RTA_CODE> <APP_KRA_CODE></APP_KRA_CODE> <FETCH_TYPE>I</FETCH_TYPE> </APP_PAN_INQ></APP_REQ_ROOT>".format(pan_no, birth_date),
+			"userName": "EKYC",
+			"PosCode": "1100066900",
+			"Password": "n3Xy62aLxQbXypuF0OyDiQ==",
+			"PassKey": "choice@123"
+		}
+		r = requests.get(url, params=params)
+		data = parker.data(fromstring(r.text))
+		user_kyc = frappe.get_doc({
+			"doctype": "User KYC",
+			"user": frappe.session.user,
+			"user_type": "KRA",
+			"investor_name": "investor name",
+			"father_name": data["APP_PAN_INQ"]["APP_F_NAME"],
+			"mother_name": "mother name",
+			"address": ", ".join(filter([data["APP_PAN_INQ"]["APP_PER_ADD1"], data["APP_PAN_INQ"]["APP_PER_ADD2"], data["APP_PER_ADD3"]["APP_F_NAME"]], None)),
+			"city": data["APP_PAN_INQ"]["APP_PER_CITY"],
+			"state": data["APP_PAN_INQ"]["APP_PER_STATE"],
+			"pincode": data["APP_PAN_INQ"]["APP_PER_PINCD"],
+			"mobile_number": data["APP_PAN_INQ"]["APP_MOB_NO"],
+			"choice_client_id": None,
+			"pan_no": data["APP_PAN_INQ"]["APP_PAN_NO"],
+			"aadhar_no": data["APP_PAN_INQ"]["APP_PAN_NO"]
+			})
+		user_kyc.insert(ignore_permissions=True)
+		frappe.db.commit()
+		return generateResponse(message="KRA USER KYC", data={"user_kyc": user_kyc, "response": data})
+		
+	except ValueError as e:
+		return generateResponse(status=422, message="Please enter valid date.")
+	except Exception as e:
+		return generateResponse(is_success=False, error=e)
 
 @frappe.whitelist()
 def get_pan(pan_no,birth_date):
