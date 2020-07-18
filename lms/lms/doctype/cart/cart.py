44a6fc68f8c13cca2037172301d6c9db3a224e2c
Neel Bhanushali <neal.bhanushali@atritechnocrat.in>
2020-07-19 04:50:03 +0530

    bre done

--- lms/lms/doctype/cart/cart.py
+++ lms/lms/doctype/cart/cart.py
@@ -3,8 +3,104 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Cart(Document):
-	pass
+	def before_save(self):
+		self.calculate_total()
+		self.validate_bre()
+
+	def calculate_total(self):
+		total = 0
+		for item in self.cart_items:
+			item.amount = item.pledged_quantity * item.price
+			total += item.amount
+
+			# updating security_category
+			item.set_security_category()
+
+		self.total = total
+
+	def validate_bre(self):
+		is_single_script = True if len(self.cart_items) == 1 else False
+		for item in self.cart_items:
+			concentration_rule = item.get_concentration_rule()
+			item.bre_passing = 1
+			item.bre_validation_message = None
+
+			# single script rule
+			if is_single_script:
+				if concentration_rule.is_single_script_allowed:
+					process_concentration_rule(
+						item=item, 
+						amount=item.amount,
+						rule=concentration_rule,
+						rule_type='single_script_threshold',
+						total=self.total
+					)
+				else:
+					item.bre_passing = 0
+					item.bre_validation_message = "Single script not allowed."
+
+				# continue to next item if bre fails
+				if not item.bre_passing:
+					continue
+
+			# group script rule
+			if not is_single_script:
+				category_amount_sum = 0
+				for i in self.cart_items:
+					if i.security_category == item.security_category:
+						category_amount_sum += item.amount
+
+				if concentration_rule.is_group_script_limited:
+					# per script rule
+					if concentration_rule.per_script_threshold > 0:
+						process_concentration_rule(
+							item=item,
+							amount=item.amount, 
+							rule=concentration_rule,
+							rule_type='per_script_threshold',
+							total=self.total
+						)
+
+					# continue to next item if bre fails
+					if not item.bre_passing:
+						continue
+					
+					# group script rule
+					if concentration_rule.group_script_threshold > 0:
+						process_concentration_rule(
+							item=item,
+							amount=category_amount_sum, 
+							rule=concentration_rule,
+							rule_type='group_script_threshold',
+							total=self.total
+						)
+
+					# continue to next item if bre fails
+					if not item.bre_passing:
+						continue
+
+				# max script rule
+				if concentration_rule.is_group_script_max_limited:
+					if concentration_rule.group_script_max_limit > 0:
+						process_concentration_rule(
+							item=item,
+							amount=category_amount_sum, 
+							rule=concentration_rule,
+							rule_type='group_script_max_limit',
+							total=self.total
+						)
+		
+		self.bre_passing = all([item.bre_passing for item in self.cart_items])
+
+def process_concentration_rule(item, amount, rule, rule_type, total):
+	threshold = rule.get(rule_type)
+	threshold_type = rule.get("{}_type".format(rule_type))
+	threshold_amt = threshold if threshold_type == "Amount" else (threshold/100.0) * total
+	
+	if amount > threshold_amt:
+		item.bre_passing = 0
+		item.bre_validation_message = "Script Amount should not exceed {}.".format(threshold_amt)
--- lms/lms/doctype/cart_item/cart_item.py
+++ lms/lms/doctype/cart_item/cart_item.py
@@ -3,8 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class CartItem(Document):
-	pass
+	def set_security_category(self):
+		security = frappe.get_doc("Allowed Security Master", self.isin)
+		self.security_category = security.category
+
+	def get_concentration_rule(self):
+		return frappe.get_doc("Concentration Rule", self.security_category)
