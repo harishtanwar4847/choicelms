358729f45dd8e258983028a14c9fdca9dc5f4764
Neel Bhanushali <neal.bhanushali@atritechnocrat.in>
2020-07-25 19:22:00 +0530

    user kyc before_save changed to before_update

--- lms/lms/doctype/user_kyc/user_kyc.py
+++ lms/lms/doctype/user_kyc/user_kyc.py
@@ -8,28 +8,28 @@ from frappe import _
 from frappe.model.document import Document
 
 class UserKYC(Document):
-	def before_save(self):
+	def before_update(self):
 		new_dict = self.as_dict()
 		old_dict = frappe.get_doc('User KYC', self.name).as_dict()
 		changes = []
-		if '__islocal' not in new_dict.keys():
-			for key in new_dict.keys():
-				if key not in ['creation', 'modified']:
-					if new_dict[key] != old_dict[key]:
-						changes.append({
-							'parameter': key, 
-							'old_value': old_dict[key], 
-							'new_value': new_dict[key]
-						})
 
-			if len(changes) == 0:
-				frappe.throw(_('No changes'))
+		for key in new_dict.keys():
+			if key not in ['creation', 'modified']:
+				if new_dict[key] != old_dict[key]:
+					changes.append({
+						'parameter': key, 
+						'old_value': old_dict[key], 
+						'new_value': new_dict[key]
+					})
 
-			user_kyc_change_application = frappe.get_doc({
-				'doctype': 'User KYC Change Application',
-				'user_kyc': self.name,
-				'changes': changes
-			})
-			user_kyc_change_application.insert(ignore_permissions=True)
-			frappe.db.commit()
-			frappe.throw(_('User KYC Change Application {} created').format(user_kyc_change_application.name))
+		if len(changes) == 0:
+			frappe.throw(_('No changes'))
+
+		user_kyc_change_application = frappe.get_doc({
+			'doctype': 'User KYC Change Application',
+			'user_kyc': self.name,
+			'changes': changes
+		})
+		user_kyc_change_application.insert(ignore_permissions=True)
+		frappe.db.commit()
+		frappe.throw(_('User KYC Change Application {} created').format(user_kyc_change_application.name))
