4ba2bc862321dfda1ee0018a4c9a6d65ffbfc42f
Neel Bhanushali <neal.bhanushali@atritechnocrat.in>
2020-07-25 16:08:58 +0530

    developer name changed

--- lms/hooks.py
+++ lms/hooks.py
@@ -4,11 +4,11 @@ from . import __version__ as app_version
 
 app_name = "lms"
 app_title = "Lms"
-app_publisher = "SMB SOlutions"
+app_publisher = "Atrina Technologies Pvt. Ltd."
 app_description = "Loan Managment System"
 app_icon = "octicon octicon-file-directory"
 app_color = "grey"
-app_email = "devloper@smb"
+app_email = "developers@atritechnocrat.com"
 app_license = "MIT"
 
 # Includes in <head>
--- lms/lms/doctype/volation_type/volation_type.py
+++ lms/lms/doctype/volation_type/volation_type.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020, SMB SOlutions and contributors
+# Copyright (c) 2020, Atrina Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
