bd155ec815cc8a79dcb3da119b708b491fef606a
Neel Bhanushali <neal.bhanushali@atritechnocrat.in>
2020-07-27 23:42:06 +0530

    las settings % validation

--- lms/lms/doctype/las_settings/las_settings.py
+++ lms/lms/doctype/las_settings/las_settings.py
@@ -3,10 +3,18 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class LASSettings(Document):
+	def before_save(self):
+		if self.loan_margin == 0:
+			frappe.throw(_('Loan margin can not be 0.'))
+
+		if self.loan_interest == 0:
+			frappe.throw(_('Loan interest can not be 0.'))
+
 	def cdsl_headers(self):
 		return {
 	 		"Referer": self.cdsl_referrer,
