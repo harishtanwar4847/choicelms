d88c257906a50518ca13af857a68834ead374987
Neel Bhanushali <neal.bhanushali@atritechnocrat.in>
2020-08-10 11:46:38 +0530

    feat(Cart): security name set in cart item

--- lms/lms/doctype/cart_item/cart_item.py
+++ lms/lms/doctype/cart_item/cart_item.py
@@ -7,9 +7,10 @@ import frappe
 from frappe.model.document import Document
 
 class CartItem(Document):
-	def set_security_category(self):
+	def set_security_details(self):
 		security = frappe.get_doc("Allowed Security", self.isin)
 		self.security_category = security.category
+		self.security_name = security.security_name
 
 	def get_concentration_rule(self):
 		return frappe.get_doc("Concentration Rule", self.security_category)
