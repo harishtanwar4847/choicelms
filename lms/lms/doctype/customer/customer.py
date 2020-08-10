701f3cb3c69490347bd8d591f0e1d1ea8a3e154c
sayyed <halima.sayyed@atritechnocrat.in>
2020-08-10 21:07:24 +0530

    update customer details

--- lms/lms/doctype/customer/customer.py
+++ lms/lms/doctype/customer/customer.py
@@ -3,8 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Customer(Document):
-	pass
+	def before_save(self):
+		user = frappe.get_doc('User', self.username)
+
+		self.first_name = user.first_name
+		self.middle_name = user.middle_name
+		self.last_name = user.last_name
+		self.full_name = user.full_name
+		self.email = user.email
+		self.phone = user.phone
