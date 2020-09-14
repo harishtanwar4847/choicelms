783f70b28af97a78751112fd5b7e831c0b279921
Neel Bhanushali <neal.bhanushali@atritechnocrat.in>
2020-09-14 14:46:50 +0530

    login api fixed

--- lms/lms/doctype/user_token/user_token.py
+++ lms/lms/doctype/user_token/user_token.py
@@ -11,12 +11,12 @@ import lms
 class UserToken(Document):
 	def after_insert(self):
 		if self.token_type in ['OTP', 'Pledge OTP']:
-			user = frappe.get_doc('User', lms.get_user(self.entity))
 			if self.token_type == 'OTP':
 				mess = frappe._('Your OTP for LMS is {0}. Do not share your OTP with anyone.').format(self.token)
 			elif self.token_type == 'Pledge OTP':
+				user = frappe.get_doc('User', lms.get_user(self.entity))
 				mess = frappe._('Your Pledge OTP for LMS is {0}. Do not share your Pledge OTP with anyone.').format(self.token)
-			frappe.enqueue(method=send_sms, receiver_list=[user.username], msg=mess)
+			frappe.enqueue(method=send_sms, receiver_list=[self.entity if self.token_type == 'OTP' else user.username], msg=mess)
 		elif self.token_type == "Email Verification Token":
 			template = "/templates/emails/user_email_verification.html"
 			url = frappe.utils.get_url("/api/method/lms.auth.verify_user?token={}&user={}".format(self.token, self.entity))
