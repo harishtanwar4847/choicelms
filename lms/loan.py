0b48d5ee82d5e180d022866706f13ca3763fe7c0
sayyed <halima.sayyed@atritechnocrat.in>
2020-09-19 13:06:40 +0530

    fix

--- lms/loan.py
+++ lms/loan.py
@@ -3,10 +3,10 @@ from frappe import _
 import lms
 
 @frappe.whitelist()
-def list(mobile):
+def my_loan(frappe.session.user):
     try:
         user = lms.get_user(frappe.session.user)
-        customer = frappe.db.get_value('Customer', {'username': user}, 'name')
+        customer = lms.get_customer(user)
 
         if not customer:
             return lms.generateResponse(message=_('User not registered.'))
