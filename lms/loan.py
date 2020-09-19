eae0c6f8bc86391f09bf96a0d0486d450d0f5e64
sayyed <halima.sayyed@atritechnocrat.in>
2020-09-19 13:00:10 +0530

    fix  api

--- lms/loan.py
+++ lms/loan.py
@@ -5,27 +5,17 @@ import lms
 @frappe.whitelist()
 def list(mobile):
     try:
-        if not mobile:
-			raise lms.ValidationError(_('Mobile is required.'))
-
-        if len(mobile) != 10 or not mobile.isdigit():
-			raise lms.ValidationError(_('Enter valid Mobile.'))
-
-        customer = frappe.db.get_value('Customer', {'user': mobile}, 'name')
+        user = lms.get_user(frappe.session.user)
+        customer = frappe.db.get_value('Customer', {'username': user}, 'name')
 
         if not customer:
             return lms.generateResponse(message=_('User not registered.'))
 
         loan = frappe.db.get_value('Loan', {'customer': customer}, 'name')
 
-        if not loan:
-            return lms.generateResponse(message=_('Loan not created for this user.'))
-        else:    
-            return lms.generateResponse(message=_('Loan'))
+        return lms.generateResponse(message=_('Loan'), data={'loan': loan})
 
     except (lms.ValidationError, lms.ServerError) as e:
         return lms.generateResponse(status=e.http_status_code, message=str(e))
     except Exception as e:
-        return lms.generateResponse(is_success=False, error=e)    
-
-
+        return lms.generateResponse(is_success=False, error=e)
