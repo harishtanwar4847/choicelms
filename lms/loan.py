8a57ea4eb1b9a84c220242c6acbcc39d036e67fe
sayyed <halima.sayyed@atritechnocrat.in>
2020-09-19 15:26:23 +0530

    return loan_margin_short_fall_value

--- lms/loan.py
+++ lms/loan.py
@@ -3,13 +3,17 @@ from frappe import _
 import lms
 
 @frappe.whitelist()
-def my_loan(frappe.session.user):
+def my_loans():
     try:
         customer = lms.get_customer(frappe.session.user)
 
-        loan = frappe.db.get_all('Loan', filters = {'customer': customer})
+        loans = frappe.db.sql(""" select loan.total_collateral_value as loan_total_collateral_value,
+            loan.name, mrgloan.total_collateral_value as loan_margin_shortfall_total_collateral_value, mrgloan.shortfall, 
+            mrgloan.shortfall_percentage, mrgloan.shortfall_c from `tabLoan` as loan
+            left join `tabLoan Margin Shortfall` as mrgloan
+            on loan.name = mrgloan.loan where loan.customer = '{}' """.format(customer.name), as_dict = 1)
 
-        return lms.generateResponse(message=_('Loan'), data={'loan': loan})
+        return lms.generateResponse(message=_('Loan'), data={'loans': loans})
 
     except (lms.ValidationError, lms.ServerError) as e:
         return lms.generateResponse(status=e.http_status_code, message=str(e))
