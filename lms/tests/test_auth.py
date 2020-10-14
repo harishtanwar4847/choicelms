dd107aef0d77a8a3fa91afe3c3233239b543426e
Iccha Konalkar <iccha.konalkar@atritechnocrat.in>
2020-10-14 11:56:53 +0530

    otp expired test case

--- lms/tests/test_auth.py
+++ lms/tests/test_auth.py
@@ -38,8 +38,19 @@ class VerifyOTP(utils.APITestCase):
 		self.assertNotFound(res)
 		self.assertRegex(res.text, 'User not found.')
 
+	def test_otp_expired(self):
+		otp = lms.create_user_token(entity=self.mobile, token=lms.random_token(length=4, is_numeric=True))
+		from datetime import datetime, timedelta
+		otp.expiry = datetime.now() - timedelta(minutes=1)
+		otp.save(ignore_permissions=True)
+		frappe.db.commit()
+		res = self.client.post_api('lms.auth.verify_otp', {'otp': otp.token, 'firebase_token': 'asdf', 'mobile': self.mobile})
+		self.assertUnauthorized(res)
+		self.assertRegex(res.text, 'OTP Expired.')
+
 	def test_wrong_otp(self):
 		res = self.client.post_api('lms.auth.verify_otp', {'otp': 1234, 'firebase_token': 'asdf', 'mobile': self.mobile})
+		print(res.text)
 		self.assertUnauthorized(res)
 		self.assertRegex(res.text, 'Invalid OTP.')
 
