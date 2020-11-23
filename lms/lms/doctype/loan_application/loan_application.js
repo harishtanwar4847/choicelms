25d3089ce0474bf9026712405a89c4ea35ff2c8b
Neel Bhanushali <neel@atriina.com>
2020-11-23 15:00:44 +0000

    SL-302 and SL-303: loan application fixed

--- lms/lms/doctype/loan_application/loan_application.js
+++ lms/lms/doctype/loan_application/loan_application.js
@@ -3,6 +3,7 @@
 
 frappe.ui.form.on('Loan Application', {
 	refresh: function(frm) {
+		cur_frm.set_df_property("items", "read_only", 1);
 		// if(['Esign Done', 'Approved'].includes(frm.doc.status)) {
 		// 	if(frm.doc.customer_esigned_document) {
 		// 		frm.set_df_property('customer_esigned_document', 'disabled', 1)
