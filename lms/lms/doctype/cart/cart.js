d15be881c1bcaf009732771ca319d94c6889fb01
Neel Bhanushali <neel@atriina.com>
2020-11-23 15:57:30 +0000

    SL-310: fixed

--- lms/lms/doctype/cart/cart.js
+++ lms/lms/doctype/cart/cart.js
@@ -3,7 +3,7 @@
 
 frappe.ui.form.on('Cart', {
 	refresh: function(frm) {
-		cur_frm.set_df_property("items", "read_only", 1);
+		frm.set_df_property("items", "read_only", 1);
 		if (!frm.doc.is_processed && false) {
 			frm.add_custom_button(__('Process dummy cart'), function() {
 				frappe.call({
