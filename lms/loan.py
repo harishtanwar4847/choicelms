8b6221895be812ed3328cc1cbad2b059aa967688
Iccha Konalkar <iccha.konalkar@atritechnocrat.in>
2020-11-28 14:06:45 +0530

    changes in loan details api - done

--- lms/loan.py
+++ lms/loan.py
@@ -334,22 +334,23 @@ def loan_details(**kwargs):
 		
 		current_date = datetime.now()
 		due_date = ""
-		due_btn_txt = ""
+		due_date_txt = "Pay By"
 		info_msg = ""
 
 		rebate_threshold = int(loan.get_rebate_threshold())
 		default_threshold = int(loan.get_default_threshold())
 		if rebate_threshold:
 			due_date =  current_date.replace(day=1) + timedelta(days=rebate_threshold) 
+			info_msg = """Interest becomes due and payable on the last date of every month. Please pay within {0} days to enjoy rebate which has already been applied while calculating the Interest Due.  After {0} days, the interest is recalculated without appliying applicable rebate and the difference appears as "Additional Interest" in your loan account. If interest remains unpaid after {1} days from the end of the month, "Penal Interest Charges" are debited to the account. Please check your terms and conditions of sanction for details.""".format(rebate_threshold, default_threshold)
 
 			if current_date > due_date:
-				due_btn_txt = "Immediate"
-				info_msg = """Interest becomes due and payable on the last date of every month. Please pay within {0} days to enjoy rebate which has already been applied while calculating the Interest Due.  After {0} days, the interest is recalculated without appliying applicable rebate and the difference appears as "Additional Interest" in your loan account. If interest remains unpaid after {1} days from the end of the month, "Penal Interest Charges" are debited to the account. Please check your terms and conditions of sanction for details.""".format(rebate_threshold, default_threshold)
+				due_date_txt = "Immediate"
+				
 
 		interest = {
 			'total_interest_amt' : interest_total[0]['total_amt'],
 			'due_date':due_date,
-			'due_btn_txt':due_btn_txt,
+			'due_date_txt':due_date_txt,
 			'info_msg':info_msg
 		}
 
