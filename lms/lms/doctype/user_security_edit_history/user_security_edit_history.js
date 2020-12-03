7a6c75983ac6e5e9f226aacbd0509661b3cd6603
Iccha Konalkar <iccha.konalkar@atritechnocrat.in>
2020-12-03 14:09:58 +0530

    removed unnecessary doctypes

--- lms/lms/doctype/user_security_edit_history/user_security_edit_history.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, Atrina Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('User Security Edit History', {
-	// refresh: function(frm) {
-
-	// }
-});
