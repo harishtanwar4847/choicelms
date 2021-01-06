a5b5671e9396944b876f8359f33424738c0fa709
Iccha Konalkar <iccha.konalkar@atriina.com>
2021-01-06 20:21:27 +0530

    create tnc file

--- lms/cart.py
+++ lms/cart.py
@@ -864,9 +864,9 @@ def get_tnc(**kwargs):
             "<li><strong> Legal & incidental charges </strong>: As per actuals;</li></ul>"
         )
 
-        url = ""
+        cart.create_tnc_file()
         tnc_header = "Please refer to the <a href='{}'>Terms & Conditions</a> for LAS facility, for detailed terms.".format(
-            url
+            frappe.utils.get_url("files/tnc/{}.pdf".format(cart.name))
         )
         tnc_footer = "You shall be required to authenticate (in token of you having fully read and irrevocably and unconditionally accepted and authenticated) the above application for loan including the pledge request and the Terms and Conditions (which can be opened by clicking on the links) and entire contents thereof, by entering the OTP that will be sent to you next on your registered mobile number with CDSL."
         tnc_checkboxes = [
@@ -880,7 +880,7 @@ def get_tnc(**kwargs):
         ]
 
         res = {
-            "tnc_list": [],  # will be removed
+            # "tnc_file": frappe.utils.get_url(tnc_file),
             "tnc_html": "".join(tnc_ul),
             "tnc_header": tnc_header,
             "tnc_footer": tnc_footer,
