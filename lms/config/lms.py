6028cf5fad7506e8da1dc84c3e91ebc3e3c88326
Neel Bhanushali <neel@atriina.com>
2021-01-07 12:15:20 +0000

    lms module dashboard updated

--- lms/config/lms.py
+++ lms/config/lms.py
@@ -10,9 +10,11 @@ def get_data():
             "items": [
                 {"type": "doctype", "name": "Lender", "onboard": 1},
                 {"type": "doctype", "name": "Allowed Security", "onboard": 1},
+                {"type": "doctype", "name": "Security", "onboard": 1},
                 {"type": "doctype", "name": "Security Category", "onboard": 1},
-                {"type": "doctype", "name": "Terms and Conditions", "onboard": 1},
                 {"type": "doctype", "name": "Security Price", "onboard": 1},
+                {"type": "doctype", "name": "Terms and Conditions", "onboard": 1},
+                {"type": "doctype", "name": "Consent", "onboard": 1},
             ],
         },
         {
@@ -20,7 +22,7 @@ def get_data():
             "items": [
                 {"type": "doctype", "name": "LAS Settings", "onboard": 1},
                 {"type": "doctype", "name": "Concentration Rule", "onboard": 1},
-                {"type": "doctype", "name": "Margin Shortfall Action", "onboard": 1},
+                # {"type": "doctype", "name": "Margin Shortfall Action", "onboard": 1},
                 {"type": "doctype", "name": "SMS Settings", "onboard": 1},
             ],
         },
@@ -32,15 +34,17 @@ def get_data():
             ],
         },
         {
-            "label": _("User Details"),
+            "label": _("User/Customer Details"),
             "items": [
                 {"type": "doctype", "name": "User", "onboard": 1},
+                {"type": "doctype", "name": "Customer", "onboard": 1},
                 {
                     "type": "doctype",
                     "name": "Approved Terms and Conditions",
                     "onboard": 1,
                 },
                 {"type": "doctype", "name": "User KYC", "onboard": 1},
+                {"type": "doctype", "name": "User Consent", "onboard": 1},
             ],
         },
         {
