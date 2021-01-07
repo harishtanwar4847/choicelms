6f1b860dc742fa2c9e4885765950e655f9eb99fc
Neel Bhanushali <neel@atriina.com>
2021-01-07 22:36:51 +0000

    user.py cleaned | user.js verified

--- cypress/integration/user.js
+++ cypress/integration/user.js
@@ -1,48 +1,80 @@
-context("User API", () => {
+context("Set PIN", () => {
   var token = null;
   before(() => {
-    cy.admin_api_call("frappe.client.delete", {
-      doctype: "User",
-      name: "0000000000@example.com",
-    });
-    cy.api_call(
-      "lms.auth.register",
-      {
-        first_name: "abcd",
-        last_name: "efgh",
-        mobile: "0000000000",
-        email: "0000000000@example.com",
-        firebase_token: "asdf",
-      },
-      "POST"
-    ).then((res) => {
+    cy.delete_dummy_user();
+    cy.register_dummy_user().then((res) => {
       token = res.body.data.token;
     });
   });
-  it("only get http method should be allowed(KYC)", () => {
+
+  it("auth method", () => {
+    cy.api_call(
+      "lms.user.set_pin",
+      { pin: Cypress.config("dummy_user").pin },
+      "POST"
+    ).then((res) => {
+      expect(res.status).to.eq(403);
+      cy.screenshot();
+    });
+  });
+
+  it("only post http method should be allowed", () => {
+    cy.api_call(
+      "lms.user.set_pin",
+      { pin: Cypress.config("dummy_user").pin },
+      "GET",
+      {
+        Authorization: token,
+      }
+    ).then((res) => {
+      expect(res.status).to.eq(405);
+      expect(res.body).to.have.property("message", "Method not allowed");
+      cy.screenshot();
+    });
+  });
+
+  it("valid hit", () => {
+    cy.api_call(
+      "lms.user.set_pin",
+      { pin: Cypress.config("dummy_user").pin },
+      "POST",
+      {
+        Authorization: token,
+      }
+    ).then((res) => {
+      expect(res.status).to.eq(200);
+      expect(res.body).to.have.property("message", "User PIN has been set");
+      cy.screenshot();
+    });
+  });
+});
+
+context("KYC", () => {
+  var token = null;
+  before(() => {
+    cy.delete_dummy_user();
+    cy.register_dummy_user().then((res) => {
+      token = res.body.data.token;
+    });
+  });
+
+  it("only get http method should be allowed", () => {
     cy.api_call("lms.user.kyc", {}, "POST", { Authorization: token }).then(
       (res) => {
         expect(res.status).to.eq(405);
         expect(res.body).to.have.property("message", "Method not allowed");
-        expect(res.body.message).to.be.a("string");
         cy.screenshot();
       }
     );
   });
-  it("KYC not found", () => {
-    cy.api_call(
-      "lms.user.kyc",
-      { pan_no: "ABCD2795", birth_date: "12-12-1999", accept_terms: true },
-      "GET",
-      { Authorization: token }
-    ).then((res) => {
-      expect(res.status).to.eq(404);
-      // expect(res.body).to.eq({});
-      expect(res.body).to.have.property("message", "User KYC not found");
-      expect(res.body.message).to.be.a("string");
+
+  it("auth method", () => {
+    cy.api_call("lms.user.kyc", {}, "GET").then((res) => {
+      expect(res.status).to.eq(403);
       cy.screenshot();
     });
   });
+
   it("field empty pan no", () => {
     cy.api_call(
       "lms.user.kyc",
@@ -53,7 +85,6 @@ context("User API", () => {
       expect(res.status).to.eq(422);
       // expect(res.body).to.eq({});
       expect(res.body).to.have.property("message", "Validation Error");
-      expect(res.body.message).to.be.a("string");
       expect(res.body).to.have.property("errors");
       expect(res.body.errors).to.be.a("object");
       expect(res.body.errors).to.have.property("pan_no");
@@ -61,6 +92,7 @@ context("User API", () => {
       cy.screenshot();
     });
   });
+
   it("field empty birth date", () => {
     cy.api_call(
       "lms.user.kyc",
@@ -71,7 +103,6 @@ context("User API", () => {
       expect(res.status).to.eq(422);
       // expect(res.body).to.eq({});
       expect(res.body).to.have.property("message", "Validation Error");
-      expect(res.body.message).to.be.a("string");
       expect(res.body).to.have.property("errors");
       expect(res.body.errors).to.be.a("object");
       expect(res.body.errors).to.have.property("birth_date");
@@ -79,6 +110,7 @@ context("User API", () => {
       cy.screenshot();
     });
   });
+
   it("accept terms false", () => {
     cy.api_call(
       "lms.user.kyc",
@@ -92,10 +124,24 @@ context("User API", () => {
         "message",
         "Please accept Terms and Conditions."
       );
-      expect(res.body.message).to.be.a("string");
       cy.screenshot();
     });
   });
+
+  it("KYC not found", () => {
+    cy.api_call(
+      "lms.user.kyc",
+      { pan_no: "ABCD2795", birth_date: "12-12-1999", accept_terms: true },
+      "GET",
+      { Authorization: token }
+    ).then((res) => {
+      expect(res.status).to.eq(404);
+      // expect(res.body).to.eq({});
+      expect(res.body).to.have.property("message", "User KYC not found");
+      cy.screenshot();
+    });
+  });
+
   it("Valid User KYC hit", () => {
     cy.api_call(
       "lms.user.kyc",
@@ -106,21 +152,44 @@ context("User API", () => {
       expect(res.status).to.eq(200);
       // expect(res.body).to.eq({});
       expect(res.body).to.have.property("message", "Success");
-      expect(res.body.message).to.be.a("string");
       cy.screenshot();
     });
   });
-  it("only get http method should be allowed(securities)", () => {
+});
+
+context("Securities", () => {
+  var token = null;
+  before(() => {
+    cy.delete_dummy_user();
+    cy.register_dummy_user().then((res) => {
+      token = res.body.data.token;
+    });
+  });
+
+  it("only get http method should be allowed", () => {
     cy.api_call("lms.user.securities", {}, "POST", {
       Authorization: token,
     }).then((res) => {
       expect(res.status).to.eq(405);
       expect(res.body).to.have.property("message", "Method not allowed");
-      expect(res.body.message).to.be.a("string");
       cy.screenshot();
     });
   });
+
+  it("auth method", () => {
+    cy.api_call("lms.user.securities", {}, "POST").then((res) => {
+      expect(res.status).to.eq(403);
+      cy.screenshot();
+    });
+  });
+
   it("Valid Securities hit", () => {
+    cy.api_call(
+      "lms.user.kyc",
+      { pan_no: "AAKHR7426K", birth_date: "01-01-1970", accept_terms: true },
+      "GET",
+      { Authorization: token }
+    );
     cy.api_call("lms.user.securities", {}, "GET", {
       Authorization: token,
     }).then((res) => {
