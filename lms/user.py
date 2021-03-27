685a92be21c3572106aff357e60ae4765b82c7f2
Iccha Konalkar <iccha.konalkar@atriina.com>
2021-03-27 19:49:50 +0530

    approved securities api - request parameter issue

--- lms/user.py
+++ lms/user.py
@@ -335,6 +335,13 @@ def approved_securities(**kwargs):
                 "is_download": "",
             },
         )
+        # if isinstance(data.get("per_page"),str):
+        #     data["per_page"] = int(data.get("per_page"))
+
+        if isinstance(data.get("is_download"), str):
+            data["is_download"] = int(data.get("is_download"))
+
+        print(kwargs, data)
 
         if not data.get("lender"):
             data["lender"] = frappe.get_last_doc("Lender").name
