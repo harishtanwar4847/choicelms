f7a1f8fc6c15d2850cde70277a92420c26502113
Iccha Konalkar <iccha.konalkar@atriina.com>
2021-04-15 20:47:46 +0530

    unpledge request api related change - wip

--- lms/lms/doctype/unpledge_application/unpledge_application.py
+++ lms/lms/doctype/unpledge_application/unpledge_application.py
@@ -9,4 +9,75 @@ from frappe.model.document import Document
 
 
 class UnpledgeApplication(Document):
-    pass
+    def get_loan(self):
+        return frappe.get_doc("Loan", self.loan)
+
+    def before_insert(self):
+        self.process_items()
+
+    def before_save(self):
+        self.process_items()
+        self.process_sell_items()
+
+    def process_items(self):
+        self.total_collateral_value = 0
+        loan = self.get_loan()
+        self.lender = loan.lender
+        self.customer = loan.customer
+
+        securities_list = [i.isin for i in self.items]
+
+        query = """
+            SELECT
+                security_name, isin, price
+            FROM
+                `tabSecurity`
+            WHERE
+                isin in {};
+        """.format(
+            lms.convert_list_to_tuple_string(securities_list)
+        )
+
+        res_ = frappe.db.sql(query, as_dict=1)
+        res = {i.isin: i for i in res_}
+
+        for i in self.items:
+            i.security_name = res.get(i.isin).security_name
+            i.price = res.get(i.isin).price
+            i.amount = i.quantity * i.price
+            self.total_collateral_value += i.amount
+
+    def process_sell_items(self):
+        if self.status != "Pending":
+            return
+
+        self.unpledge_collateral_value = 0
+
+        price_map = {i.isin: i.price for i in self.items}
+        unpledge_quantity_map = {i.isin: 0 for i in self.items}
+
+        for i in self.unpledge_items:
+            if i.unpledge_quantity > i.quantity:
+                frappe.throw(
+                    "Can not sell {}(PSN: {}) more than {}".format(
+                        i.isin, i.psn, i.quantity
+                    )
+                )
+            unpledge_quantity_map[i.isin] = (
+                unpledge_quantity_map[i.isin] + i.unpledge_quantity
+            )
+            self.unpledge_collateral_value += i.unpledge_quantity * price_map.get(
+                i.isin
+            )
+
+        for i in self.items:
+            if unpledge_quantity_map.get(i.isin) > i.quantity:
+                frappe.throw("Can not sell {} more than {}".format(i.isin, i.quantity))
+
+
+@frappe.whitelist()
+def get_collateral_details(unpledge_application_name):
+    doc = frappe.get_doc("Unpledge Application", unpledge_application_name)
+    loan = doc.get_loan()
+
+    return loan.get_collateral_list(group_by_psn=True)
