59937ad0ea15c1d7f79e9dc1cc861373cbe44222
Omkar Darves <omkar.darves@atriina.com>
2021-06-26 15:49:58 +0000

    pdf excel notification changes

--- lms/loan.py
+++ lms/loan.py
@@ -1839,7 +1839,22 @@ def loan_statement(**kwargs):
                 loan_statement_notification = loan_statement_notification.replace(
                     "investor_name", user_kyc.investor_name
                 )
-
+                loan_statement_notification = loan_statement_notification.replace(
+                    "logo_file", frappe.utils.get_url('/assets/lms/mail_images/logo.png')
+                )
+                loan_statement_notification = loan_statement_notification.replace(
+                    "fb_icon", frappe.utils.get_url('/assets/lms/mail_images/fb-icon.png')
+                )
+                loan_statement_notification = loan_statement_notification.replace(
+                    "tw_icon", frappe.utils.get_url('/assets/lms/mail_images/tw-icon.png')
+                )
+                loan_statement_notification = loan_statement_notification.replace(
+                    "inst_icon", frappe.utils.get_url('/assets/lms/mail_images/inst-icon.png')
+                )
+                loan_statement_notification = loan_statement_notification.replace(
+                    "lin_icon", frappe.utils.get_url('/assets/lms/mail_images/lin-icon.png')
+                )
+                
                 res["is_mail_sent"] = 1
                 frappe.enqueue(
                     method=frappe.sendmail,
