67c598663a85a37c08f2d4b9970f26d77dd53067
deep.chirag <deep.chirag@atriina.com>
2021-07-06 13:17:02 +0530

    working query on search

--- lms/www/newsblogs.py
+++ lms/www/newsblogs.py
@@ -24,24 +24,29 @@ def fetch_blogs_all(page_no):
 def search_blog(search_content, page_no):
     limit = 6
     offset = (int(page_no) - 1) * limit
-    # if search_content:
-    # search_content = pymysql.escape_string("'")
-    print(search_content)
-    searched_blogs = frappe.db.sql(
-        """select nb.*, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) where nb.title = '{}' group by nb.name limit {},6""".format(
-            str(search_content), offset
-        ),
-        as_dict=True,
-        debug=True,
-    )
+    if search_content:
+        print(search_content)
+        searched_blogs = frappe.db.sql(
+            "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) where nb.title LIKE %(txt)s  group by nb.name limit {},6".format(
+                offset
+            ),
+            {
+                "txt": "%{}%".format(search_content),
+            },
+            as_dict=True,
+            debug=True,
+        )
 
-    response = {"searched_blogs": searched_blogs, "page_no": page_no}
-    # else:
-    #     searched_blogs = frappe.db.sql(
-    #     "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) group by nb.name limit {},6".format(offset),
-    #     as_dict=True,
-    #     debug=True,
-    #     )
-    #     response = {"searched_blogs": searched_blogs, "page_no": page_no}
+        response = {"searched_blogs": searched_blogs, "page_no": page_no}
+    else:
+        searched_blogs = frappe.db.sql(
+            "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) group by nb.name limit {},6".format(
+                offset
+            ),
+            as_dict=True,
+            debug=True,
+        )
+
+        response = {"searched_blogs": searched_blogs, "page_no": page_no}
 
     return utils.respondWithSuccess(data=response)
