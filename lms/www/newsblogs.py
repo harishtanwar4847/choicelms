af8057016bd623a288920152a8cc02844fccfdb3
deep.chirag <deep.chirag@atriina.com>
2021-07-07 23:44:03 +0530

    Worked on Search functionality and SQL Query

--- lms/www/newsblogs.py
+++ lms/www/newsblogs.py
@@ -1,10 +1,7 @@
-from datetime import datetime
-
 import frappe
 import utils
 
 
-# import lms
 def get_context(context):
     context.blogs = frappe.get_all(
         "News and Blog", fields=["*"], page_length=6, order_by="creation desc"
@@ -17,76 +14,37 @@ def get_context(context):
 def fetch_blogs(page_no, latest_post="", trending_post="", search_key=""):
     limit = 6
     offset = (int(page_no) - 1) * limit
-    if latest_post == "true":
-        if len(search_key.strip()) > 0:
-            where = "where nb.title LIKE %(txt)s and nb.is_latest=true"
-            where_extras = {"txt": "%{}%".format(search_key)}
-            blogs_all = frappe.db.sql(
-                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
-                    where, offset, limit
-                ),
-                {"format": "%d %M, %Y"},
-                where_extras,
-                as_dict=True,
-                debug=True,
-            )
-        else:
-            where = "where nb.is_latest=true"
-            blogs_all = frappe.db.sql(
-                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date,Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb. name) {} group by nb.name order by creation desc limit {},{}".format(
-                    where, offset, limit
-                ),
-                {"format": "%d %M, %Y"},
-                as_dict=True,
-                debug=True,
-            )
-        response = {"blogs_all": blogs_all, "page_no": page_no}
-    elif trending_post == "true":
-        if len(search_key.strip()) > 0:
-            where = "where nb.title LIKE %(txt)s and nb.is_trending=true"
-            where_extras = {"txt": "%{}%".format(search_key)}
-            blogs_all = frappe.db.sql(
-                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
-                    where, offset, limit
-                ),
-                {"format": "%d %M, %Y"},
-                where_extras,
-                as_dict=True,
-                debug=True,
-            )
-        else:
-            where = "where nb.is_trending=true"
-            blogs_all = frappe.db.sql(
-                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb. name) {} group by nb.name order by creation desc limit {},{}".format(
-                    where, offset, limit
-                ),
-                {"format": "%d %M, %Y"},
-                as_dict=True,
-                debug=True,
-            )
-        response = {"blogs_all": blogs_all, "page_no": page_no}
-    else:
-        if len(search_key.strip()) > 0:
-            where = "where nb.title LIKE %(txt)s"
-            where_extras = {"txt": "%{}%".format(search_key)}
-            blogs_all = frappe.db.sql(
-                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
-                    where, offset, limit
-                ),
-                {"format": "%d %M, %Y"},
-                where_extras,
-                as_dict=True,
-                debug=True,
-            )
-        else:
-            blogs_all = frappe.db.sql(
-                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) group by nb.name order by creation desc limit {},{}".format(
-                    offset, limit
-                ),
-                {"format": "%d %M, %Y"},
-                as_dict=True,
-                debug=True,
-            )
+    date_format = "DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date,"
+    if len(search_key.strip()) > 0:
+        where = "where nb.title LIKE %(txt)s"
+        if latest_post == "true":
+            where += " and nb.is_latest=true"
+        elif trending_post == "true":
+            where += " and nb.is_trending=true"
 
-        response = {"blogs_all": blogs_all, "page_no": page_no}
+        where_extras = {"format": "%d %M, %Y", "txt": "%{}%".format(search_key)}
+
+        blogs_all = frappe.db.sql(
+            "select nb.name,nb.title,nb.for_banner_view, nb.creation, {} Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) {} group by nb.name order by creation desc limit {},{}".format(
+                date_format, where, offset, limit
+            ),
+            where_extras,
+            as_dict=True,
+        )
+
+    else:
+        where = ""
+        if latest_post == "true":
+            where += "where nb.is_latest=true"
+        elif trending_post == "true":
+            where += "where nb.is_trending=true"
+
+        blogs_all = frappe.db.sql(
+            "select nb.name,nb.title,nb.for_banner_view,nb.creation, {} Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) {} group by nb.name order by creation desc limit {},{}".format(
+                date_format, where, offset, limit
+            ),
+            {"format": "%d %M, %Y"},
+            as_dict=True,
+        )
+    response = {"blogs_all": blogs_all, "page_no": page_no}
     return utils.respondWithSuccess(data=response)
