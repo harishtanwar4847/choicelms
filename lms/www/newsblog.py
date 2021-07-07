ec149c07387f907ba8a0c1f3bb6ebc1010feeb6b
deep.chirag <deep.chirag@atriina.com>
2021-07-07 20:55:14 +0530

    Search Function and Filters

--- lms/www/newsblog.py
+++ lms/www/newsblog.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from re import DEBUG
+
 import frappe
 import utils
 
@@ -5,27 +8,33 @@ import lms
 
 
 def get_context(context):
-    # TODO : get all tags of main blog
-    # TODO : apply in query to filter related blogs
-    # TODO : ajax method
-    # TODO : handle view more html(received through ajax response)
-    # TODO : ajax method with search key
-    # TODO : manage search html(received through ajax response)
-
-    blog_details = frappe.get_doc("News and Blog", frappe.form_dict.blog_name)
-    tags_list = [i.website_tags for i in blog_details.blog_tags]
+    context.blog_details = frappe.get_doc(
+        "News and Blog", frappe.form_dict.blog_name
+    ).as_dict()
+    context.blog_details.publishing_date = (
+        context.blog_details.publishing_date
+    ).strftime("%d %B, %Y")
+    tags_list = [i.website_tags for i in context.blog_details.blog_tags]
 
     # context.related_articles = frappe.db.sql(
     #     "select nb.title, nb.publishing_date, GROUP_CONCAT(bt.website_tags) as website_tags, nb.for_banner_view from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) where nb.name <> '{}' AND website_tags in {} group by nb.name".format(frappe.form_dict.blog_name, lms.convert_list_to_tuple_string(tags_list)),
     # as_dict=True, debug=True)
-
     context.related_articles = frappe.db.sql(
-        "select nb.title, nb.publishing_date, GROUP_CONCAT(bt.website_tags) as website_tags, nb.for_banner_view from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) where nb.name in (Select nb.name from `tabNews and Blog` nb, `tabBlog Tags` bt where bt.parent=nb.name AND nb.name <> '{}' AND bt.website_tags in {} group by nb.name) group by nb.name order by nb.creation desc limit 3;".format(
+        "select nb.title, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, GROUP_CONCAT(bt.website_tags) as website_tags, nb.for_banner_view from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) where nb.name in (Select nb.name from `tabNews and Blog` nb, `tabBlog Tags` bt where bt.parent=nb.name AND nb.name <> '{}' AND bt.website_tags in {} group by nb.name) group by nb.name order by nb.creation desc limit 3;".format(
             frappe.form_dict.blog_name, lms.convert_list_to_tuple_string(tags_list)
         ),
-        as_dict=True,
+        {"format": "%d %M, %Y"},
         debug=True,
+        as_dict=True,
     )
+    # context.page_change = ""
+    context.page_change = frappe.db.sql(
+        "SELECT name, (SELECT name FROM `tabNews and Blog` nb1 WHERE nb1.creation < nb.creation ORDER BY creation DESC LIMIT 1) as previous_name, (SELECT name FROM `tabNews and Blog` nb2 WHERE nb2.creation > nb.creation ORDER BY creation ASC LIMIT 1) as next_name FROM `tabNews and Blog` nb WHERE name = '{}'".format(
+            frappe.form_dict.blog_name
+        ),
+        as_dict=True,
+    )[0]
+    # print(context.page_change)
 
 
 @frappe.whitelist(allow_guest=True)
@@ -38,12 +47,13 @@ def fetch_related_articles(blog_name, page_no):
     offset = (int(page_no) - 1) * limit
 
     related_articles = frappe.db.sql(
-        "select nb.name, nb.title, nb.publishing_date, GROUP_CONCAT(bt.website_tags) as website_tags, nb.for_banner_view from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) where nb.name in (Select nb.name from `tabNews and Blog` nb, `tabBlog Tags` bt where bt.parent=nb.name AND nb.name <> '{}' AND bt.website_tags in {} group by nb.name) group by nb.name order by nb.creation desc limit {}, 3;".format(
+        "select nb.name, nb.title, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, GROUP_CONCAT(bt.website_tags) as website_tags, nb.for_banner_view from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) where nb.name in (Select nb.name from `tabNews and Blog` nb, `tabBlog Tags` bt where bt.parent=nb.name AND nb.name <> '{}' AND bt.website_tags in {} group by nb.name) group by nb.name order by nb.creation desc limit {}, 3;".format(
             blog_name, lms.convert_list_to_tuple_string(tags_list), offset
         ),
+        {"format": "%d %M, %Y"},
         as_dict=True,
-        debug=True,
     )
+    # print(related_articles)
 
     response = {"related_articles": related_articles, "page_no": page_no}
 
--- lms/www/newsblogs.py
+++ lms/www/newsblogs.py
@@ -1,22 +1,31 @@
+from datetime import datetime
+
 import frappe
 import utils
 
+
 # import lms
+def get_context(context):
+    context.blogs = frappe.get_all(
+        "News and Blog", fields=["*"], page_length=6, order_by="creation desc"
+    )
+    for blog in context.blogs:
+        blog.publishing_date = (blog.publishing_date).strftime("%d %B, %Y")
 
 
 @frappe.whitelist(allow_guest=True)
-def fetch_blogs(page_no, latest_post="", search_key=""):
-    limit = 3
+def fetch_blogs(page_no, latest_post="", trending_post="", search_key=""):
+    limit = 6
     offset = (int(page_no) - 1) * limit
     if latest_post == "true":
-
         if len(search_key.strip()) > 0:
             where = "where nb.title LIKE %(txt)s and nb.is_latest=true"
             where_extras = {"txt": "%{}%".format(search_key)}
             blogs_all = frappe.db.sql(
-                "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
+                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
                     where, offset, limit
                 ),
+                {"format": "%d %M, %Y"},
                 where_extras,
                 as_dict=True,
                 debug=True,
@@ -24,9 +33,34 @@ def fetch_blogs(page_no, latest_post="", search_key=""):
         else:
             where = "where nb.is_latest=true"
             blogs_all = frappe.db.sql(
-                "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb. name) {} group by nb.name order by creation desc limit {},{}".format(
+                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date,Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb. name) {} group by nb.name order by creation desc limit {},{}".format(
                     where, offset, limit
                 ),
+                {"format": "%d %M, %Y"},
+                as_dict=True,
+                debug=True,
+            )
+        response = {"blogs_all": blogs_all, "page_no": page_no}
+    elif trending_post == "true":
+        if len(search_key.strip()) > 0:
+            where = "where nb.title LIKE %(txt)s and nb.is_trending=true"
+            where_extras = {"txt": "%{}%".format(search_key)}
+            blogs_all = frappe.db.sql(
+                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
+                    where, offset, limit
+                ),
+                {"format": "%d %M, %Y"},
+                where_extras,
+                as_dict=True,
+                debug=True,
+            )
+        else:
+            where = "where nb.is_trending=true"
+            blogs_all = frappe.db.sql(
+                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb. name) {} group by nb.name order by creation desc limit {},{}".format(
+                    where, offset, limit
+                ),
+                {"format": "%d %M, %Y"},
                 as_dict=True,
                 debug=True,
             )
@@ -36,18 +70,20 @@ def fetch_blogs(page_no, latest_post="", search_key=""):
             where = "where nb.title LIKE %(txt)s"
             where_extras = {"txt": "%{}%".format(search_key)}
             blogs_all = frappe.db.sql(
-                "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
+                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
                     where, offset, limit
                 ),
+                {"format": "%d %M, %Y"},
                 where_extras,
                 as_dict=True,
                 debug=True,
             )
         else:
             blogs_all = frappe.db.sql(
-                "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) group by nb.name order by creation desc limit {},{}".format(
+                "select nb.*, DATE_FORMAT(nb.publishing_date, %(format)s) as publishing_date, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) group by nb.name order by creation desc limit {},{}".format(
                     offset, limit
                 ),
+                {"format": "%d %M, %Y"},
                 as_dict=True,
                 debug=True,
             )
