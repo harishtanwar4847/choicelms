f2dd8a8f9ca0b6393cffa29a32a6c7b2cbf0507b
deep.chirag <deep.chirag@atriina.com>
2021-07-07 10:48:32 +0530

    worked on filter and search function

--- lms/www/newsblogs.py
+++ lms/www/newsblogs.py
@@ -5,29 +5,52 @@ import utils
 
 
 @frappe.whitelist(allow_guest=True)
-def fetch_blogs(page_no, search_key=""):
+def fetch_blogs(page_no, latest_post="", search_key=""):
     limit = 3
     offset = (int(page_no) - 1) * limit
+    if latest_post == "true":
 
-    if len(search_key.strip()) > 0:
-        where = "where nb.title LIKE %(txt)s"
-        where_extras = {"txt": "%{}%".format(search_key)}
-        blogs_all = frappe.db.sql(
-            "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb. name) {} group by nb.name order by creation desc limit {},{}".format(
-                where, offset, limit
-            ),
-            where_extras,
-            as_dict=True,
-            debug=True,
-        )
+        if len(search_key.strip()) > 0:
+            where = "where nb.title LIKE %(txt)s and nb.is_latest=true"
+            where_extras = {"txt": "%{}%".format(search_key)}
+            blogs_all = frappe.db.sql(
+                "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
+                    where, offset, limit
+                ),
+                where_extras,
+                as_dict=True,
+                debug=True,
+            )
+        else:
+            where = "where nb.is_latest=true"
+            blogs_all = frappe.db.sql(
+                "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb. name) {} group by nb.name order by creation desc limit {},{}".format(
+                    where, offset, limit
+                ),
+                as_dict=True,
+                debug=True,
+            )
+        response = {"blogs_all": blogs_all, "page_no": page_no}
     else:
-        blogs_all = frappe.db.sql(
-            "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb. name) group by nb.name order by creation desc limit {},{}".format(
-                offset, limit
-            ),
-            as_dict=True,
-            debug=True,
-        )
+        if len(search_key.strip()) > 0:
+            where = "where nb.title LIKE %(txt)s"
+            where_extras = {"txt": "%{}%".format(search_key)}
+            blogs_all = frappe.db.sql(
+                "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) {} group by nb.name order by creation desc limit {},{}".format(
+                    where, offset, limit
+                ),
+                where_extras,
+                as_dict=True,
+                debug=True,
+            )
+        else:
+            blogs_all = frappe.db.sql(
+                "select nb.*, Group_CONCAT(bt.website_tags) as website_tags from    `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent =   nb. name) group by nb.name order by creation desc limit {},{}".format(
+                    offset, limit
+                ),
+                as_dict=True,
+                debug=True,
+            )
 
-    response = {"blogs_all": blogs_all, "page_no": page_no}
+        response = {"blogs_all": blogs_all, "page_no": page_no}
     return utils.respondWithSuccess(data=response)
