9eaae52f288d45f44f99720492c0536bf1fbeb80
Omkar Darves <omkar.darves@atriina.com>
2021-08-24 14:06:13 +0000

    after_migrate hook added for new doctype data import

--- lms/patches/import_spark_push_notification_content.py
+++ lms/patches/import_spark_push_notification_content.py
@@ -2,12 +2,15 @@ import frappe
 
 
 def execute():
-    frappe.db.sql("TRUNCATE `tabSpark Push Notification`")
+    doc_exists = frappe.db.sql(
+        "SELECT EXISTS(SELECT 1 FROM `tabSpark Push Notification`) as OUTPUT;",
+        as_dict=True,
+    )
 
-    frappe.db.commit()
-    path = frappe.get_app_path(
-        "lms", "patches", "imports", "spark_push_notification.csv"
-    )
-    frappe.core.doctype.data_import.data_import.import_file(
-        "Spark Push Notification", path, "Insert"
-    )
+    if not doc_exists[0].get("OUTPUT"):
+        path = frappe.get_app_path(
+            "lms", "patches", "imports", "spark_push_notification.csv"
+        )
+        frappe.core.doctype.data_import.data_import.import_file(
+            "Spark Push Notification", path, "Insert"
+        )
