42cafca7968792900438c13b67feabd6fb94ea19
deep.chirag <deep.chirag@atriina.com>
2021-09-02 14:46:42 +0530

    changes in Currency format in words in TNC template, Loan Agreement & Loan Enhance Agreement

--- lms/__init__.py
+++ lms/__init__.py
@@ -702,6 +702,110 @@ def validate_rupees(type_of_fees):
     process_charge = str(process_charge)
     arr = process_charge.split(".")
 
+    if arr[1] == "0":
+        return int(type_of_fees)
+    else:
+        return "{:.2f}".format(float(type_of_fees))
+
+
+def validate_percent(type_of_fees):
+    process_charge = type_of_fees
+    process_charge = str(process_charge)
+    arr = process_charge.split(".")
+
+    if arr[1] == "0":
+        return int(type_of_fees)
+    else:
+        return "{:.2f}".format(float(type_of_fees))
+
+
+def number_to_word(number):
+    def get_word(n):
+        words = {
+            0: "",
+            1: "One",
+            2: "Two",
+            3: "Three",
+            4: "Four",
+            5: "Five",
+            6: "Six",
+            7: "Seven",
+            8: "Eight",
+            9: "Nine",
+            10: "Ten",
+            11: "Eleven",
+            12: "Twelve",
+            13: "Thirteen",
+            14: "Fourteen",
+            15: "Fifteen",
+            16: "Sixteen",
+            17: "Seventeen",
+            18: "Eighteen",
+            19: "Nineteen",
+            20: "Twenty",
+            30: "Thirty",
+            40: "Forty",
+            50: "Fifty",
+            60: "Sixty",
+            70: "Seventy",
+            80: "Eighty",
+            90: "Ninty",
+        }
+        if n <= 20:
+            return words[n]
+        else:
+            ones = n % 10
+            tens = n - ones
+            return words[tens] + " " + words[ones]
+
+    def get_all_word(n):
+        d = [100, 10, 100, 100]
+        v = ["", "Hundred And", "Thousand", "lakh"]
+        w = []
+        for i, x in zip(d, v):
+            t = get_word(n % i)
+            if t != "":
+                t += " " + x
+            w.append(t.rstrip(" "))
+            n = n // i
+        w.reverse()
+        w = " ".join(w).strip()
+        if w.endswith("And"):
+            w = w[:-3]
+        return w
+
+    number1 = float(number)
+    arr = str(number).split(".")
+    number = int(arr[0])
+    crore = number // 10000000
+    number = number % 10000000
+    word = ""
+    if number1 > 1:
+        if crore > 0:
+            word += get_all_word(crore)
+            word += " crore "
+        word += "Rupees " + get_all_word(number).strip()
+        if len(arr) > 1:
+            if len(arr[1]) == 1:
+                arr[1] += "0"
+            word += " and " + get_all_word(int(arr[1])) + " paise"
+    elif number1 == 1:
+        if crore > 0:
+            word += get_all_word(crore)
+            word += " crore "
+        word += "Rupee " + get_all_word(number).strip()
+        if len(arr) > 1:
+            if len(arr[1]) == 1:
+                arr[1] += "0"
+            word += " and " + get_all_word(int(arr[1])) + " paise"
+    elif number == 0:
+        if len(arr) > 1:
+            if len(arr[1]) == 1:
+                arr[1] += "0"
+            # word +="Rupees "+ get_all_word(int(arr[1])) + " paise"
+            word += get_all_word(int(arr[1])) + " paise"
+    return word
+
 
 @frappe.whitelist(allow_guest=True)
 def nsdl_success_callback(**kwargs):
