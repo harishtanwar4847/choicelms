10d9d0fa0a8efadebb858cb1026c877af02ba4f6
poonam <poonam.ghuge@atriina.com>
2021-09-18 19:08:48 +0530

    Feat: backend screen loan summary

--- lms/lms/doctype/loan_transaction/loan_transaction.js
+++ lms/lms/doctype/loan_transaction/loan_transaction.js
@@ -5,5 +5,110 @@ frappe.ui.form.on("Loan Transaction", {
   refresh: function (frm) {
     let is_allowed = frappe.user_roles.includes("System Manager");
     frm.toggle_enable(["transaction_type", "amount"], is_allowed);
+
+    frappe.db.get_doc("Loan", frm.doc.loan).then((doc) => {
+      var data = "";
+      data +=
+        "<style>" +
+        "table" +
+        "{" +
+        "border-collapse:separate;border:solid black 2px;border-radius:6px;-moz-border-radius:6px;background-color:white;width:100%" +
+        "}" +
+        "td,th" +
+        "{" +
+        "border-left:solid black 2px;border-top:solid black 2px;text-align: center;color:black;padding:6px 4px;" +
+        "}" +
+        "th" +
+        "{" +
+        "border-top: none;" +
+        "}" +
+        "td:first-child, th:first-child" +
+        "{" +
+        " border-left: none;" +
+        "}" +
+        "</style>" +
+        "<table>" +
+        "<tr>" +
+        "<th>" +
+        "Loan Account No" +
+        "</th>" +
+        "<th>" +
+        "Customer Name" +
+        "</th>" +
+        "<th>" +
+        "Loan Balance" +
+        "</th>" +
+        "<th>" +
+        "Virtual interest not booked" +
+        "</th>" +
+        "<th>" +
+        "Drawing Power" +
+        "</th>" +
+        "<th>" +
+        "Sanctioned limit" +
+        "</th>" +
+        "<th>" +
+        "Lender name" +
+        "</th>" +
+        "<th>" +
+        "Margin shortfall" +
+        "</th>" +
+        "<th>" +
+        "Interest due" +
+        "</th>" +
+        "<th>" +
+        "Interest overdue" +
+        "</th>" +
+        "<th>" +
+        "DPD(days past due)" +
+        "</th>" +
+        "<th>" +
+        "Collateral value" +
+        "</th>" +
+        "</tr>" +
+        "<tr>" +
+        "<td>" +
+        doc.name +
+        "</td>" +
+        "<td>" +
+        doc.customer_name +
+        "</td>" +
+        "<td>" +
+        doc.balance +
+        "</td>" +
+        "<td>" +
+        doc.base_interest_amount +
+        "</td>" +
+        "<td>" +
+        doc.drawing_power +
+        "</td>" +
+        "<td>" +
+        doc.sanctioned_limit +
+        "</td>" +
+        "<td>" +
+        doc.lender +
+        "</td>" +
+        "<td>" +
+        doc.margin_shortfall_amount +
+        "</td>" +
+        "<td>" +
+        "-" +
+        "</td>" +
+        "<td>" +
+        "-" +
+        "</td>" +
+        "<td>" +
+        "-" +
+        "</td>" +
+        "<td>" +
+        doc.total_collateral_value +
+        "</td>" +
+        "</tr>";
+      ("</table>");
+      frm.set_intro(data);
+    });
+    document.getElementsByClassName("form-message")[0].style.background =
+      "#f9fafa";
+    document.getElementsByClassName("form-message")[0].style.padding = "0px";
   },
 });
