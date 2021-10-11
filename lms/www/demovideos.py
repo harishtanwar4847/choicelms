ec5e7f140e194fa80e7d44cb3e31611c8da8227b
poonam <poonam.ghuge@atriina.com>
2021-10-11 16:35:42 +0530

    Pushing all files related about demo videos

--- lms/www/demovideos.py
+++ lms/www/demovideos.py
@@ -12,16 +12,21 @@ def get_videos_list(page_no, latest_video="", search_key=""):
     else:
         limit = 6
     offset = (int(page_no) - 1) * limit
-    where = ""
-    if len(search_key.strip()) > 0:
-        where += "where video_title LIKE '%(txt)s' or video_description LIKE '%(txt)s'"
 
-    videos_list = frappe.db.sql(
-        "select video_title,youtube_id,video_description from `tabYoutube Id` {} order by modified desc limit {},{}".format(
-            where, offset, limit
-        ),
-        as_dict=True,
-    )
+    if len(search_key.strip()) > 0:
+        videos_list = frappe.db.sql(
+            "select video_title,youtube_id,video_description from `tabYoutube Id` where video_title LIKE '%{}%' or video_description LIKE '%{}%' order by modified desc limit {},{}".format(
+                search_key, search_key, offset, limit
+            ),
+            as_dict=True,
+        )
+    else:
+        videos_list = frappe.db.sql(
+            "select video_title,youtube_id,video_description from `tabYoutube Id` order by modified desc limit {},{}".format(
+                offset, limit
+            ),
+            as_dict=True,
+        )
 
     response = {"videos_list_response": videos_list, "page_no": page_no}
 
