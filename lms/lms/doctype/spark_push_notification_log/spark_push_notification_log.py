2a07a33339394ca7dc198bd42560bbee9d1d4355
Deep Chirag <deep.chirag@atriina.com>
2021-10-11 16:26:22 +0530

    Autoname set

--- lms/lms/doctype/spark_push_notification_log/spark_push_notification_log.py
+++ lms/lms/doctype/spark_push_notification_log/spark_push_notification_log.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2021, Atrina Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SparkPushNotificationLog(Document):
-    pass
+    def autoname(self):
+        self.name = "SPNL-{}-{}".format(
+            self.notification_id, str(self.time).replace(" ", "-")
+        )
