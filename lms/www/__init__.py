0aec813bca143007e5ac3935a3eee96fd1ec6e25
Deep Chirag <deep.chirag@atriina.com>
2021-11-10 16:04:26 +0530

    Feat: View More functionality

--- lms/www/__init__.py
+++ lms/www/__init__.py
@@ -13,30 +13,34 @@ def fetch_blogs(page_no, latest_post="", trending_post="", search_key=""):
             where += " and nb.is_latest=true"
         elif trending_post == "true":
             where += " and nb.is_trending=true"
-
         where_extras = {"format": "%d %M, %Y", "txt": "%{}%".format(search_key)}
-
-        blogs_all = frappe.db.sql(
-            "select nb.route,nb.name,nb.title,nb.for_banner_view, nb.creation, {} Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) {} group by nb.name order by creation desc limit {},{}".format(
-                date_format, where, offset, limit
-            ),
-            where_extras,
-            as_dict=True,
-        )
-
     else:
         where = ""
         if latest_post == "true":
             where += "where nb.is_latest=true"
         elif trending_post == "true":
             where += "where nb.is_trending=true"
+        where_extras = {"format": "%d %M, %Y"}
 
-        blogs_all = frappe.db.sql(
-            "select nb.route,nb.name,nb.title,nb.for_banner_view,nb.creation, {} Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) {} group by nb.name order by creation desc limit {},{}".format(
-                date_format, where, offset, limit
-            ),
-            {"format": "%d %M, %Y"},
-            as_dict=True,
-        )
-    response = {"blogs_all": blogs_all, "page_no": page_no}
+    blogs_all = frappe.db.sql(
+        "select nb.route,nb.name,nb.title,nb.for_banner_view,nb.creation, {} Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) {} group by nb.name order by creation desc limit {},{}".format(
+            date_format, where, offset, limit
+        ),
+        where_extras,
+        as_dict=True,
+    )
+
+    count_all_blogs = frappe.db.sql(
+        "select count(*) as all_blogs, nb.route,nb.name,nb.title,nb.for_banner_view,nb.creation, {} Group_CONCAT(bt.website_tags) as website_tags from `tabNews and Blog` as nb left join `tabBlog Tags` bt on (bt.parent = nb.name) {} group by nb.name order by creation desc".format(
+            date_format, where
+        ),
+        where_extras,
+        as_dict=True,
+    )
+
+    response = {
+        "blogs_all": blogs_all,
+        "page_no": page_no,
+        "count_all_blogs": len(count_all_blogs),
+    }
     return utils.respondWithSuccess(data=response)
