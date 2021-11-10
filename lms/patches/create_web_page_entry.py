53c1af070626258e677d4a0324d2508190e54f0d
Deep Chirag <deep.chirag@atriina.com>
2021-11-10 12:10:58 +0530

    Feat: Patches for news and blog entry

--- lms/patches/create_web_page_entry.py
+++ lms/patches/create_web_page_entry.py
@@ -7,34 +7,38 @@ from frappe.website.utils import cleanup_page_name
 def execute():
     las_settings = frappe.get_single("LAS Settings")
     news_blog_template = las_settings.get_news_blog_template()
-    # blogs = frappe.get_all("News and Blog", filters=["*"])
-    # for blog in blogs:
-    #     frappe.db.sql("""
-    #     update `tabNews and Blog`
-    #     set name = '{name}'
-    #     where name = '{name}'
-    #     """.format(name = cleanup_page_name(blog.title).replace("_", "-")))
-    #     # blog.name = cleanup_page_name(blog.title).replace("_", "-")
-    #     # blog.save()
-    #     frappe.db.commit()
-    # blog_details = {
-    #     "title": blog.title,
-    #     "blog_tags": blog.blog_tags,
-    #     "author": blog.author,
-    #     "publishing_date": blog.publishing_date,
-    #     "for_blog_view": blog.for_blog_view,
-    #     "description": blog.description,
-    # }
-    # html = frappe.render_template(
-    #     news_blog_template.get_content(), {"blog_details": blog_details}
-    # )
-    # web_page = frappe.new_doc("Web Page")
-    # web_page.title = blog.title
-    # web_page.route = "news-and-blogs/" + blog.route
-    # web_page.content_type = "HTML"
-    # web_page.dynamic_template = True
-    # web_page.main_section_html = html
-    # web_page.meta_title = blog.meta_title
-    # web_page.meta_description = blog.meta_description
-    # web_page.meta_image = blog.meta_image
-    # web_page.insert(ignore_permissions=True)
+    blogs = frappe.get_all("News and Blog", fields=["*"])
+
+    for blog in blogs:
+        frappe.db.sql(
+            """
+        update `tabNews and Blog`
+        set name = '{new_name}', route = '{new_name}'
+        where name = '{name}'
+        """.format(
+                new_name=cleanup_page_name(blog.title).replace("_", "-"), name=blog.name
+            )
+        )
+        frappe.db.commit()
+
+        blog_details = {
+            "title": blog.title,
+            "blog_tags": blog.blog_tags,
+            "author": blog.author,
+            "publishing_date": blog.publishing_date,
+            "for_blog_view": blog.for_blog_view,
+            "description": blog.description,
+        }
+        html = frappe.render_template(
+            news_blog_template.get_content(), {"blog_details": blog_details}
+        )
+        web_page = frappe.new_doc("Web Page")
+        web_page.title = blog.title
+        web_page.route = "news-and-blogs/" + blog.name
+        web_page.content_type = "HTML"
+        web_page.dynamic_template = True
+        web_page.main_section_html = html
+        web_page.meta_title = blog.title
+        web_page.meta_description = blog.short_description
+        web_page.meta_image = blog.for_blog_view
+        web_page.insert(ignore_permissions=True)
