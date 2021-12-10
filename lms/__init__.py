d2f42ffde0c4de920a8b8c70af6e12a2409f3571
Omkar Darves <omkar.darves@atriina.com>
2021-12-10 09:49:58 +0000

    add enqueue function to resolve integrity error in loan transaction creation

--- lms/__init__.py
+++ lms/__init__.py
@@ -998,10 +998,11 @@ def rzp_payment_webhook_callback(**kwargs):
                 # "request data json": json.dumps(frappe.local.request.data, separators = (',', ':')),
                 "request_data_str": frappe.local.request.data.decode(),
                 "type_of_form_dict": type(frappe.local.form_dict),
+                "form_dict": frappe.local.form_dict,
                 "request": frappe.local.request,
-                "form_dict.verification_str":client.utility.verify_webhook_signature(str(frappe.local.form_dict), webhook_signature, webhook_secret),
+                "req_data_verification_str":client.utility.verify_webhook_signature(str(frappe.local.request.data), webhook_signature, webhook_secret),
                 "req_data_str_utf8":client.utility.verify_webhook_signature(str(frappe.local.request.data, 'utf-8'), webhook_signature, webhook_secret),
-                "json_dumps_separators":client.utility.verify_webhook_signature(json.dumps(frappe.local.form_dict, separators=(",",":")), webhook_signature, webhook_secret)
+                "json_dumps_separators":client.utility.verify_webhook_signature(json.dumps(frappe.local.request.data, separators=(",",":")), webhook_signature, webhook_secret)
             }      
 
             create_log(log, "rzp_payment_webhook_log")
