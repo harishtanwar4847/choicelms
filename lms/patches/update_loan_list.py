42102cc5791e162a49c822e1d80a704547b74615
Deep Chirag <deep.chirag@atriina.com>
2021-12-13 13:41:24 +0530

    Feat: patch update for dpd, interest due, overdue, penal, total interest

--- lms/patches/update_loan_list.py
+++ lms/patches/update_loan_list.py
@@ -25,21 +25,22 @@ def execute():
                 loan.name, loan.name
             )
         )
-        # frappe.db.sql(
-        #     "update `tabLoan` set interest_due = IFNULL((select unpaid_interest from `tabLoan Transaction` where loan = '{}' and transaction_type = 'Interest' and unpaid_interest > 0 order by time desc limit 1),0.0) where name = '{}'".format(
-        #         loan.name, loan.name
-        #     )
-        # )
-        # frappe.db.sql(
-        #     "update `tabLoan` set interest_overdue = IFNULL((select sum(unpaid_interest) as unpaid_interest from `tabLoan Transaction` where loan = '{}' and transaction_type in ('Interest', 'Additional Interest') and unpaid_interest >0),0.0) where name = '{}'".format(
-        #         loan.name, loan.name
-        #     )
-        # )
-        # frappe.db.sql(
-        #     "update `tabLoan` set penal_interest_charges = IFNULL((select sum(unpaid_interest) as unpaid_interest from `tabLoan Transaction` where loan = '{}' and transaction_type = 'Penal Interest' and unpaid_interest >0),0.0) where name = '{}'".format(
-        #         loan.name, loan.name
-        #     )
-        # )
+        # select unpaid_interest from `tabLoan Transaction` where loan = 'SL000025' and transaction_type = 'Interest' and unpaid_interest > 0 and (select unpaid_interest from `tabLoan Transaction` where loan = "SL000025" and transaction_type = "Additional Interest" and DATE_FORMAT(time, '%Y') = "2021" and DATE_FORMAT(time, '%m') = "12") <=0 order by time desc;
+        frappe.db.sql(
+            "update `tabLoan` set interest_due = IFNULL((select unpaid_interest from `tabLoan Transaction` where loan = '{}' and transaction_type = 'Interest' and transaction_type = (select ) and unpaid_interest > 0 order by time desc limit 1),0.0) where name = '{}'".format(
+                loan.name, loan.name
+            )
+        )
+        frappe.db.sql(
+            "update `tabLoan` set interest_overdue = IFNULL((select sum(unpaid_interest) as unpaid_interest from `tabLoan Transaction` where loan = '{}' and transaction_type in ('Interest', 'Additional Interest') and unpaid_interest >0),0.0) where name = '{}'".format(
+                loan.name, loan.name
+            )
+        )
+        frappe.db.sql(
+            "update `tabLoan` set penal_interest_charges = IFNULL((select sum(unpaid_interest) as unpaid_interest from `tabLoan Transaction` where loan = '{}' and transaction_type = 'Penal Interest' and unpaid_interest >0),0.0) where name = '{}'".format(
+                loan.name, loan.name
+            )
+        )
         frappe.db.sql(
             "update `tabLoan` set total_interest_incl_penal_due = IFNULL((select sum(unpaid_interest) as total_amount from `tabLoan Transaction` where loan = '{}' and transaction_type in ('Interest', 'Additional Interest', 'Penal Interest') and unpaid_interest >0),0.0) where name = '{}'".format(
                 loan.name, loan.name
