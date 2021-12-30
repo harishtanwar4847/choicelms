b71abf3cdad6443157d06e1dc5eb82b5723beefa
Iccha Konalkar <iccha.konalkar@atriina.com>
2021-12-30 11:56:31 +0530

    Feat:Jiffy KYC API Integration-patch code-done

--- lms/patches/update_jiffy_kyc_details.py
+++ lms/patches/update_jiffy_kyc_details.py
@@ -1,60 +1,22 @@
 from datetime import datetime
 
 import frappe
-import utils
-from frappe import _
 
-from lms import __user_kyc, user
 from lms.user import request_choice_kyc
 
 
 def execute():
     frappe.reload_doc("Lms", "DocType", "User KYC")
+    frappe.reload_doc("Lms", "DocType", "User Bank Account")
+
     user_kyc_list = frappe.get_all("User KYC", fields=["*"])
     for kyc in user_kyc_list:
         try:
             res_json = request_choice_kyc({"pan_no": kyc.pan_no})
-            # user_kyc = __user_kyc(entity=kyc.user, pan_no=kyc.pan_no, throw=False)
 
             # update kyc details
             frappe.db.sql(
-                """Update `tabUser KYC`
-                set
-                kyc_type = "CHOICE",
-                investor_name='{}',
-                first_name='{}',
-                middle_name='{}',
-                last_name='{}',
-                email_id='{}',
-                date_of_birth='{}',
-                father_name='{}',
-                mother_name='{}',
-                address='{}',
-                permanant_address1='{}',
-                permanant_address2='{}',
-                permanant_address3='{}',
-                per_city='{}',
-                per_state='{}',
-                per_pincode='{}',
-                current_address1='{}',
-                current_address2='{}',
-                current_address3='{}',
-                curr_city='{}',
-                curr_state='{}',
-                curr_pincode='{}',
-                address_city='{}',
-                address_state='{}',
-                address_pincode='{}',
-                city='{}',
-                state='{}',
-                pincode='{}',
-                address_proof_type='{}',
-                same_as_current_address='{}',
-                mobile_number='{}',
-                choice_client_id='{}',
-                pan_no='{}'
-                where name='{}'
-                """.format(
+                """Update `tabUser KYC` set kyc_type = "CHOICE", investor_name='{}', first_name='{}', middle_name='{}', last_name='{}', email_id='{}', date_of_birth='{}', father_name='{}', mother_name='{}', address='{}', permanant_address1='{}',                 permanant_address2='{}', permanant_address3='{}', per_city='{}', per_state='{}', per_pincode='{}', current_address1='{}', current_address2=' {}', current_address3='{}', curr_city='{}', curr_state='{}', curr_pincode='{}', address_city='{}', address_state='{}', address_pincode='{}',city='{}', state='{}', pincode='{}', address_proof_type='{}', same_as_current_address={}, mobile_number='{}', choice_client_id='{}', pan_no='{}' where name='{}'""".format(
                     res_json["investorName"],
                     res_json["firstName"],
                     res_json["middleName"],
@@ -99,7 +61,8 @@ def execute():
                     1 if res_json["sameAsCurrentAddress"] == "true" else 0,
                     res_json["mobileNum"],
                     res_json["clientId"],
-                    res_json["panNum"].kyc.name,
+                    res_json["panNum"],
+                    kyc.name,
                 )
             )
 
@@ -111,27 +74,8 @@ def execute():
                     )
                     if user_bank_acc_name:
                         frappe.db.sql(
-                            """Update `tabUser Bank Account` set
-                        is_default='{}',
-                        account_type='{}',
-                        cancel_cheque_file_name='{}',
-                        ifsc='{}',
-                        micr='{}',
-                        branch='{}',
-                        bank='{}',
-                        bank_address='{}',
-                        contact='{}',
-                        city='{}',
-                        district='{}',
-                        state='{}',
-                        bank_mode='{}',
-                        bank_code='{}',
-                        bank_zip_code='{}',
-                        address_state='{}',
-                        account_number='{}'
-                        where name='{}' and parent='{}' and account_number='{}'
-                        """.format(
-                                bank["defaultBank"] == "Y",
+                            """Update `tabUser Bank Account` set is_default={}, account_type='{}', cancel_cheque_file_name='{}', ifsc='{}', micr='{}', branch='{}', bank='{}', bank_address='{}', contact='{}', city='{}', district='{}', state='{}', bank_mode='{}', bank_code='{}', bank_zip_code='{}', address_state='{}', account_number='{}' where name='{}' and parent='{}' and account_number='{}'""".format(
+                                1 if bank["defaultBank"] == "Y" else 0,
                                 bank["accountType"],
                                 bank["cancelChequeFileName"],
                                 bank["ifsc"],
