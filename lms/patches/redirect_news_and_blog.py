106919243724afe6634861b10fdb32cda2573e18
Deep Chirag <deep.chirag@atriina.com>
2022-01-27 13:07:55 +0530

    Feat: 301 url redirect of 404 pages under /newsblog

--- lms/patches/redirect_news_and_blog.py
+++ lms/patches/redirect_news_and_blog.py
@@ -2,18 +2,14 @@ import frappe
 
 
 def execute():
-    source_target_dict = [
-        {"source": "newsblog", "target": "/news-and-blogs"},
-    ]
-    for dict in source_target_dict:
-        frappe.get_doc(
-            {
-                "doctype": "Website Route Redirect",
-                "source": dict["source"],
-                "target": dict["target"],
-                "parent": "Website Settings",
-                "parenttype": "Website Settings",
-                "parentfield": "route_redirects",
-            }
-        ).insert()
+    frappe.get_doc(
+        {
+            "doctype": "Website Route Redirect",
+            "source": "newsblog",
+            "target": "/news-and-blogs",
+            "parent": "Website Settings",
+            "parenttype": "Website Settings",
+            "parentfield": "route_redirects",
+        }
+    ).insert()
     frappe.db.commit()
