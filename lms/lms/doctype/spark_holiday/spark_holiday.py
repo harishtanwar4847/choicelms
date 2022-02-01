77cc7a86761fa7bf4783f3d0c6e94ae5fc5a7538
Omkar Darves <omkar.darves@atriina.com>
2022-02-01 05:51:12 +0000

    drawing power update in increase loan

--- lms/lms/doctype/spark_holiday/spark_holiday.py
+++ lms/lms/doctype/spark_holiday/spark_holiday.py
@@ -6,7 +6,20 @@ from frappe.model.document import Document
 
 
 class SparkHoliday(Document):
-    def validate(self):
-        duplicate_date = frappe.get_value(self.doctype, {"date": self.date}, "name")
-        if duplicate_date:
-            frappe.throw("This Date already exists.")
+    pass
+    # def validate(self):
+    #     duplicate_date = frappe.get_value(self.doctype, {"date": self.date}, "name")
+    #     if duplicate_date:
+    #         frappe.throw("This Date already exists.")
+
+    # def on_update(self):
+    #     self.update_margin_shortfall_deadline()
+
+    # def update_margin_shortfall_deadline(self):
+    #     loan_margin_shortfall = frappe.get_all("Loan Margin Shortfall", {"status": ["Pending","Request Pending"]})
+    #     if loan_margin_shortfall:
+    #         for single_margin_shortfall in loan_margin_shortfall:
+    #             single_margin_shortfall = frappe.get_doc("Loan Margin Shortfall", single_margin_shortfall.name)
+    #             single_margin_shortfall.update_deadline_based_on_holidays()
+    #             single_margin_shortfall.save(ignore_permissions=True)
+    #             frappe.db.commit()
