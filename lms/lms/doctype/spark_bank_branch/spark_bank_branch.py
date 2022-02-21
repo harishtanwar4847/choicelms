0babce0f6d75adfe036a44ef22b90f78a81f992f
vagrant <vagrant@vagrant.vm>
2022-02-21 07:27:04 +0000

    Changing validation for spark branch

--- lms/lms/doctype/spark_bank_branch/spark_bank_branch.py
+++ lms/lms/doctype/spark_bank_branch/spark_bank_branch.py
@@ -12,32 +12,39 @@ import lms
 
 class SparkBankBranch(Document):
     def validate(self):
-        self.get_bank_branch()
-        # self.get_district()
-        # self.get_city()
-        # self.get_state()
+        self.get_ifsc()
+        self.get_district()
+        self.get_city()
+        self.get_state()
 
-    def get_bank_branch(self):
-        is_alphanumeric = lms.regex_special_characters(
-            search=self.ifsc, regex=re.compile("^[a-zA-Z0-9]*$")
-        )
-        if not is_alphanumeric:
-            frappe.throw("Only Alphanumeric value are allowed.")
+    def get_ifsc(self):
+        if self.ifsc:
+            is_alphanumeric = lms.regex_special_characters(
+                search=self.ifsc, regex=re.compile("^[a-zA-Z0-9]*$")
+            )
+            if not is_alphanumeric:
+                frappe.throw("Only Alphanumeric value are allowed.")
 
-        is_alphanumeric = lms.regex_special_characters(
-            search=self.district, regex=re.compile("^[a-zA-Z0-9]*$")
-        )
-        if not is_alphanumeric:
-            frappe.throw("Only Alphanumeric value are allowed.")
+    def get_district(self):
+        if self.district:
+            is_alphanumeric = lms.regex_special_characters(
+                search=self.district, regex=re.compile("^[a-zA-Z0-9]*$")
+            )
+            if not is_alphanumeric:
+                frappe.throw("Only Alphanumeric value are allowed.")
 
-        is_alphanumeric = lms.regex_special_characters(
-            search=self.city, regex=re.compile("^[a-zA-Z0-9]*$")
-        )
-        if not is_alphanumeric:
-            frappe.throw("Only Alphanumeric value are allowed.")
+    def get_city(self):
+        if self.city:
+            is_alphanumeric = lms.regex_special_characters(
+                search=self.city, regex=re.compile("^[a-zA-Z0-9]*$")
+            )
+            if not is_alphanumeric:
+                frappe.throw("Only Alphanumeric value are allowed.")
 
-        is_alphanumeric = lms.regex_special_characters(
-            search=self.state, regex=re.compile("^[a-zA-Z0-9]*$")
-        )
-        if not is_alphanumeric:
-            frappe.throw("Only Alphanumeric value are allowed.")
+    def get_state(self):
+        if self.state:
+            is_alphanumeric = lms.regex_special_characters(
+                search=self.state, regex=re.compile("^[a-zA-Z0-9]*$")
+            )
+            if not is_alphanumeric:
+                frappe.throw("Only Alphanumeric value are allowed.")
