e73124d7541d6b871d9b10e46262446da25efd16
Omkar Darves <omkar.darves@atriina.com>
2022-03-30 06:11:11 +0000

    patches fix

--- lms/patches/update_instrument_type_in_doctype.py
+++ lms/patches/update_instrument_type_in_doctype.py
@@ -7,6 +7,7 @@ def execute():
     frappe.reload_doc("Lms", "DocType", "Loan Margin Shortfall")
     frappe.reload_doc("Lms", "DocType", "Margin Shortfall Action")
     frappe.reload_doc("Lms", "DocType", "Collateral Ledger")
+    frappe.reload_doc("Lms", "DocType", "Top up Application")
 
     frappe.db.sql("""update `tabLoan` set instrument_type = "Shares";""")
     frappe.db.sql("""update `tabLoan Transaction` set instrument_type = "Shares";""")
@@ -17,4 +18,5 @@ def execute():
         """update `tabMargin Shortfall Action` set instrument_type = "Shares";"""
     )
     frappe.db.sql("""update `tabCollateral Ledger` set instrument_type = "Shares";""")
+    frappe.db.sql("""update `tabTop up Application` set instrument_type = "Shares";""")
     frappe.db.commit()
