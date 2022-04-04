5395f4597e2d785c04067102d6326dd88d6dcf32
Omkar Darves <omkar.darves@atriina.com>
2022-04-04 06:45:26 +0000

    margin shortfall action patch

---
+++ lms/patches/update_margin_shortfall_action_list.py
@@ -0,0 +1,29 @@
+import frappe
+
+
+def execute():
+    frappe.reload_doc("Lms", "DocType", "Margin Shortfall Action")
+    all_actions = frappe.get_all("Margin Shortfall Action", ["*"])
+    if len(all_actions) == 3:
+        for a in all_actions:
+            frappe.get_doc(
+                {
+                    "doctype": "Margin Shortfall Action",
+                    "max_threshold": a.max_threshold,
+                    "instrument_type": "Mutual Fund",
+                    "scheme_type": "Debt",
+                    "sell_off_after_hours": a.sell_off_after_hours,
+                    "sell_off_deadline_eod": a.sell_off_deadline_eod,
+                }
+            ).insert(ignore_permissions=True)
+            frappe.get_doc(
+                {
+                    "doctype": "Margin Shortfall Action",
+                    "max_threshold": a.max_threshold,
+                    "instrument_type": "Mutual Fund",
+                    "scheme_type": "Equity",
+                    "sell_off_after_hours": a.sell_off_after_hours,
+                    "sell_off_deadline_eod": a.sell_off_deadline_eod,
+                }
+            ).insert(ignore_permissions=True)
+            frappe.db.commit()
