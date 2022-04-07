d8871b06220b28f8ce0c1ad2a5425fecdbbad81a
Deep Chirag <deep.chirag@atriina.com>
2022-04-07 13:40:27 +0530

    Feat: update psn in case revoke initiate and show prf fields in case of mutual fund

--- lms/lms/doctype/unpledge_application/unpledge_application.py
+++ lms/lms/doctype/unpledge_application/unpledge_application.py
@@ -518,6 +518,7 @@ def initiate_revoc(unpledge_application_name):
                     i.revoke_validate_remarks = isin_details.get(i.get("isin")).get(
                         "remarks"
                     )
+                    i.psn = isin_details.get(i.get("isin")).get("revoc_refno")
 
             if dict_decrypted_response.get("revocinitiate").get("message") == "SUCCESS":
                 doc.is_initiated = True
