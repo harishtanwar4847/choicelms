e4612e2134e784050203b1fb6ec05d8642cf209f
vagrant <vagrant@vagrant.vm>
2022-04-11 12:54:29 +0000

    Update spark push notification csv

---
+++ lms/patches/update_spark_push_notification_content.py
@@ -0,0 +1,14 @@
+import frappe
+
+
+def execute():
+    frappe.reload_doc("Lms", "DocType", "Spark Push Notification")
+
+    frappe.db.sql("truncate table `tabSpark Push Notification`")
+
+    path = frappe.get_app_path(
+        "lms", "patches", "imports", "update_spark_push_notification.csv"
+    )
+    frappe.core.doctype.data_import.data_import.import_file(
+        "Spark Push Notification", path, "Insert"
+    )
