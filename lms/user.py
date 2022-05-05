a5219b84efa4739732b0012e3592e40910a464bf
Deep Chirag <deep.chirag@atriina.com>
2022-05-05 17:24:40 +0530

    Feat: AMC code and image

--- lms/user.py
+++ lms/user.py
@@ -1366,9 +1366,10 @@ def schemes(**kwargs):
             )
 
         schemes_list = frappe.db.sql(
-            """select als.isin, als.security_name as scheme_name, als.eligible_percentage as ltv, als.instrument_type, als.scheme_type, round(s.price,4) as price, group_concat(lender,'') as lenders
+            """select als.isin, als.security_name as scheme_name, als.eligible_percentage as ltv, als.instrument_type, als.scheme_type, round(s.price,4) as price, group_concat(lender,'') as lenders, ad.amc_code, ad.amc_image
             from `tabAllowed Security` als
             LEFT JOIN `tabSecurity` s on s.isin = als.isin
+            LEFT JOIN `tabAMC Details` ad on ad.security = als.isin
             where als.instrument_type='Mutual Fund' and s.price > 0{}{}{}
             group by als.isin
             order by als.creation desc;""".format(
