c75cd2d72b64d11901a6a4e3fdef8573696e7939
Deep Chirag <deep.chirag@atriina.com>
2022-05-17 17:57:39 +0530

    Feat: file name rename of csv data

--- lms/patches/import_amc_details.py
+++ lms/patches/import_amc_details.py
@@ -4,7 +4,7 @@ import frappe
 def execute():
     frappe.db.sql("truncate `tabAMC Details`")
     frappe.db.commit()
-    path = frappe.get_app_path("lms", "patches", "imports", "AMC_Details.csv")
+    path = frappe.get_app_path("lms", "patches", "imports", "amc_code_details.csv")
     frappe.core.doctype.data_import.data_import.import_file(
         "AMC Details", path, "Insert"
     )
