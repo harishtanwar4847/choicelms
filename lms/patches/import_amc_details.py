47938196cf4242e097e8a7f6a6b526e30d0eb86f
Deep Chirag <deep.chirag@atriina.com>
2022-06-02 14:35:06 +0530

    Feat: update allowed security data, update amc master import

--- lms/patches/import_amc_details.py
+++ lms/patches/import_amc_details.py
@@ -4,7 +4,7 @@ import frappe
 def execute():
     frappe.db.sql("truncate `tabAMC Master`")
     frappe.db.commit()
-    path = frappe.get_app_path("lms", "patches", "imports", "amc_code_details.csv")
+    path = frappe.get_app_path("lms", "patches", "imports", "amc_master_details.csv")
     frappe.core.doctype.data_import.data_import.import_file(
         "AMC Master", path, "Insert"
     )
