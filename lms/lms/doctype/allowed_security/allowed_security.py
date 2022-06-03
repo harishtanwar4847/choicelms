9fc91a806afe659d5ae545e3116bc16f722debb3
Deep Chirag <deep.chirag@atriina.com>
2022-06-03 13:15:37 +0530

    Feat: Bulk insert request api call and bulk response handle

--- lms/lms/doctype/allowed_security/allowed_security.py
+++ lms/lms/doctype/allowed_security/allowed_security.py
@@ -5,6 +5,8 @@
 from __future__ import unicode_literals
 
 import json
+import random
+import string
 
 import frappe
 import requests
@@ -13,9 +15,6 @@ from frappe.model.document import Document
 from frappe.utils.csvutils import read_csv_content
 
 import lms
-
-# import utils
-# import random
 
 
 class AllowedSecurity(Document):
@@ -105,3 +104,139 @@ class AllowedSecurity(Document):
                 + "\n\nAllowed Security Update Error for isin:\n"
                 + str(self.isin),
             )
+
+
+@frappe.whitelist()
+def update_mycams_scheme_bulk(upload_file):
+    f = frappe.get_all("File", filters={"file_url": upload_file}, page_length=1)
+    f = frappe.get_doc("File", f[0].name)
+    ff = f.get_full_path()
+    with open(ff, "r") as upfile:
+        fcontent = upfile.read()
+
+    csv_data = read_csv_content(fcontent)
+
+    schemedetails = []
+    for i in csv_data[1:]:
+        scheme = {
+            "amccode": i[6],
+            "isinno": i[1],
+            "approveflag": "Y" if i[10] else "N",
+            "lienperc": i[3],
+        }
+        schemedetails.append(scheme)
+    try:
+        datetime_signature = lms.create_signature_mycams()
+        las_settings = frappe.get_single("LAS Settings")
+        headers = {
+            "Content-Type": "application/json",
+            "clientid": las_settings.client_id,
+            "datetimestamp": datetime_signature[0],
+            "signature": datetime_signature[1],
+            "subclientid": "",
+        }
+        url = las_settings.lien_allowed_scheme_update_api
+        data = {
+            "lienscheme": {
+                "clientid": las_settings.client_id,
+                "clientname": "",
+                "subclientid": "",
+                "schemedetails": schemedetails,
+            }
+        }
+
+        encrypted_data = lms.AESCBC(
+            las_settings.encryption_key, las_settings.iv
+        ).encrypt(json.dumps(data))
+        req_data = {"req": str(encrypted_data)}
+        resp = requests.post(url=url, headers=headers, data=json.dumps(req_data)).text
+        encrypted_response = (
+            json.loads(resp).get("res").replace("-", "+").replace("_", "/")
+        )
+        decrypted_response = lms.AESCBC(
+            las_settings.decryption_key, las_settings.iv
+        ).decrypt(encrypted_response)
+        dict_decrypted_response = json.loads(decrypted_response)
+        lms.create_log(
+            {
+                "json_payload": data,
+                "encrypted_request": encrypted_data,
+                "encrypred_response": json.loads(resp).get("res"),
+                "decrypted_response": dict_decrypted_response,
+            },
+            "approve_security_update",
+        )
+
+        fields = [
+            "name",
+            "isin",
+            "lender",
+            "eligible_percentage",
+            "security_category",
+            "instrument_type",
+            "amc_code",
+            "amc_name",
+            "security_name",
+            "scheme_type",
+            "allowed",
+            "creation",
+            "modified",
+            "owner",
+            "modified_by",
+            "remark",
+            "res_status",
+        ]
+        approved_security_map = {
+            i[1]: {
+                "name": "".join(
+                    random.choice(string.ascii_lowercase + string.digits)
+                    for _ in range(10)
+                ),
+                "isin": i[1],
+                "lender": i[2],
+                "eligible_percentage": i[3],
+                "security_category": i[4],
+                "instrument_type": i[5],
+                "amc_code": i[6],
+                "amc_name": i[7],
+                "security_name": i[8],
+                "scheme_type": i[9],
+                "allowed": int(i[10]),
+                "creation": frappe.utils.now(),
+                "modified": frappe.utils.now(),
+                "owner": "Administrator",
+                "modified_by": "Administrator",
+            }
+            for i in csv_data[1:]
+        }
+
+        values = []
+        lienscheme = dict_decrypted_response["lienscheme"]
+
+        if lienscheme and lienscheme["errorcode"] == "S000":
+            for i in lienscheme["schemedetails"]:
+                scheme = approved_security_map.get(i["isinno"])
+                scheme["remark"] = (
+                    "Y - " + lienscheme["error"]
+                    if scheme["allowed"]
+                    else "N - " + lienscheme["error"]
+                )
+                if i["status"] == "SUCCESS":
+                    scheme["res_status"] = True
+                else:
+                    scheme["res_status"] = False
+                    scheme["allowed"] = False if scheme["allowed"] == True else False
+
+                scheme = list(scheme.values())
+                values.append(scheme)
+        else:
+            frappe.throw(frappe._(lienscheme["error"]))
+        frappe.db.bulk_insert(
+            "Allowed Security", fields=fields, values=values, ignore_duplicates=False
+        )
+    except requests.RequestException as e:
+        frappe.log_error(
+            title="Allowed Security Update - Error",
+            message=frappe.get_traceback()
+            + "\n\nAllowed Security Update Error for isin\n",
+        )
--- lms/lms/doctype/allowed_security/allowed_security_list.js
+++ lms/lms/doctype/allowed_security/allowed_security_list.js
@@ -3,4 +3,33 @@
 
 frappe.listview_settings["Allowed Security"] = {
   hide_name_column: true,
+  refresh: function (listview) {
+    listview.page.add_inner_button(__("Upload CSV"), function () {
+      let d = new frappe.ui.Dialog({
+        title: "Enter details",
+        fields: [
+          {
+            label: "Upload CSV",
+            fieldname: "file",
+            fieldtype: "Attach",
+          },
+        ],
+        primary_action_label: "Submit",
+        primary_action(values) {
+          console.log(values);
+          frappe.call({
+            method:
+              "lms.lms.doctype.allowed_security.allowed_security.update_mycams_scheme_bulk",
+            args: {
+              upload_file: values.file,
+            },
+            freeze: true,
+          });
+          d.hide();
+        },
+      });
+
+      d.show();
+    });
+  },
 };
