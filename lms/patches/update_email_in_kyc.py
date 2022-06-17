ea13d0edb4e014ed74139639512dfbd4ccab49d7
Deep Chirag <deep.chirag@atriina.com>
2022-06-17 13:43:19 +0530

    Feat: OTP on email, patches for update email, addded email field in user kyc

---
+++ lms/patches/update_email_in_kyc.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+import frappe
+import requests
+
+import lms
+
+
+def execute():
+    frappe.reload_doc("Lms", "DocType", "User KYC")
+    user_kyc = frappe.get_all("User KYC", fields=["*"])
+    for kyc in user_kyc:
+        if not kyc["email"]:
+            las_settings = frappe.get_single("LAS Settings")
+            params = {
+                "PANNum": kyc["pan_no"],
+                "dob": kyc["date_of_birth"],
+            }
+
+            headers = {
+                "businessUnit": las_settings.choice_business_unit,
+                "userId": las_settings.choice_user_id,
+                "investorId": las_settings.choice_investor_id,
+                "ticket": las_settings.choice_ticket,
+            }
+
+            res = requests.get(
+                las_settings.choice_pan_api, params=params, headers=headers
+            )
+
+            data = res.json()
+
+            if res.ok and not "errorCode" in data:
+
+                # frappe.db.sql("""
+                # update `tabUser KYC` set email = '{}' where name = '{}'
+                # """.format(data["emailId"],kyc["name"]))
+
+                # confirmed by vinayak
+                user_kyc_doc = frappe.get_doc("User KYC", kyc["name"])
+                user_kyc_doc.email = data["emailId"]
+                user_kyc_doc.save(ignore_permissions=True)
+            frappe.db.commit()
