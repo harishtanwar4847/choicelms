59ec36a9f66d0596af2f6a0cf388f90045a20bbc
Aditi <aditi.patade@atriina.com>
2022-06-20 11:51:03 +0000

    Feat : Country Master file

--- lms/patches/import_state_master.py
+++ lms/patches/import_state_master.py
@@ -2,6 +2,7 @@ import frappe
 
 
 def execute():
+    frappe.reload_doc("Lms", "DocType", "State Master")
     path = frappe.get_app_path("lms", "patches", "imports", "state_master.csv")
     frappe.core.doctype.data_import.data_import.import_file(
         "State Master", path, "Insert", console=True
