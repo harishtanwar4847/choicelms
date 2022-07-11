b2347ae378f9c3b1450cb82a08e5b0638acf9469
Aditi <aditi.patade@atriina.com>
2022-07-11 11:45:09 +0000

    Feat : System Report Doctype creation

---
+++ lms/lms/doctype/client_sanction_details/test_client_sanction_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Atrina Technologies Pvt. Ltd. and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+
+class TestClientSanctionDetails(unittest.TestCase):
+    pass
