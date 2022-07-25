aa2a6a0daf53a4c61e849cd8d2d884562abf0744
Omkar Darves <omkar.darves@atriina.com>
2022-07-25 07:47:30 +0000

    spark app version doctype and api changes

--- lms/user.py
+++ lms/user.py
@@ -3065,3 +3065,22 @@ def contact_us(**kwargs):
         return utils.respondWithSuccess()
     except utils.exceptions.APIException as e:
         return e.respond()
+
+
+@frappe.whitelist()
+def get_app_version_details():
+    try:
+        utils.validator.validate_http_method("GET")
+
+        version_details = frappe.get_all(
+            "Spark App Version",
+            fields=["*"],
+            order_by="release_date desc",
+            page_length=1,
+        )
+        if not version_details:
+            raise lms.exceptions.NotFoundException(_("No Record found"))
+        return utils.respondWithSuccess(data=version_details[0])
+    except utils.exceptions.APIException as e:
+        lms.log_api_error()
+        return e.respond()
