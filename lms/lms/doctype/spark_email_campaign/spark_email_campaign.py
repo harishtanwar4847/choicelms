97ff370f1e5ade228694da83e6930b4807d95890
vagrant <vagrant@vagrant.vm>
2022-08-01 11:23:26 +0000

    feat:default_image,no_of_users,filter

--- lms/lms/doctype/spark_email_campaign/spark_email_campaign.py
+++ lms/lms/doctype/spark_email_campaign/spark_email_campaign.py
@@ -13,12 +13,20 @@ import lms
 
 
 class SparkEmailCampaign(Document):
-    def on_submit(self):
-        self.mail_send()
+    
 
     def validate(self):
         if len(self.sender_email) > 1:
-            frappe.throw(("Maximum 1 level allowed."))
+            frappe.throw(frappe._("Maximum 1 level allowed."))
+        if not len(self.sender_email):
+            frappe.throw(frappe._("Please select at least on sender email account"))
+
+        # for i in self.sender_email:
+        #     if not ("spark" in i.email_id or "choice" in i.email_id):
+        #         # sender = i.email_id
+        #     # else:
+        #         frappe.throw(frappe._("Please Enter Spark/Choice email id"))
+        
 
     # def on_cancel(self):
     #     print("abcd")
@@ -33,14 +41,7 @@ class SparkEmailCampaign(Document):
     #     # doc = frappe.get_all("Loan Customer",filters={"send_after":self.self.customer_selection},fields=["user"])
     #     # for i in
 
-    def before_save(self):
-        for i in self.sender_email:
-            if "spark" in i.email_id or "choice" in i.email_id:
-                sender = i.email_id
-            else:
-                frappe.throw("Please Enter Spark/Choice email id")
-
-    def mail_send(self):
+    def user_data(self):
         doc_list = []
         if self.customer_selection == "All Customer":
             doc = frappe.get_all("Loan Customer", fields=["user"])
@@ -52,28 +53,49 @@ class SparkEmailCampaign(Document):
             )
             for i in doc:
                 doc_list.append(i.user)
-        elif self.customer_selection == "":
-            doc = frappe.get_all("Loan Customer", fields=["user"])
+        elif self.customer_selection == "Non Loan Customer":
+            doc = frappe.get_all("Loan Customer", fields=["user"],
+            filters={"loan_open": 0}
+            )
             for i in doc:
                 doc_list.append(i.user)
-        elif self.customer_selection == "":
-            doc = frappe.get_all("Loan Customer", fields=["user"])
+        elif self.customer_selection == "Kyc Customer":
+            doc = frappe.get_all("Loan Customer", fields=["user"],
+            filters={"kyc_update": 1})
             for i in doc:
                 doc_list.append(i.user)
         else:
             for i in self.customer_email:
                 doc_list.append(i.user)
 
-        for i in self.sender_email:
-            if "spark" in i.email_id or "choice" in i.email_id:
-                sender = i.email_id
+        return doc_list
+    
 
-        for user in doc_list:
-            frappe.enqueue(
-                method=frappe.sendmail,
-                recipients=user,
-                sender=sender,
-                subject=self.subject,
-                message=self.template_html,
-                send_after=self.schedule_datetime,
-            )
+    def before_save(self):
+        customer_list = self.user_data()
+        self.no_of_user = len(customer_list)
+        self.logo_file = frappe.utils.get_url("/assets/lms/mail_images/logo.png")
+        self.fb_icon = frappe.utils.get_url("/assets/lms/mail_images/fb-icon.png")
+        self.tw_icon = frappe.utils.get_url("/assets/lms/mail_images/tw-icon.png")
+        self.inst_icon = frappe.utils.get_url("/assets/lms/mail_images/inst-icon.png")
+
+
+    def mail_send(self):
+        # for i in self.sender_email:
+        #     if "spark" in i.email_id or "choice" in i.email_id:
+        #         sender = i.email_id
+        customer_list = self.user_data()
+        # for user in customer_list:
+        frappe.enqueue(
+            method=frappe.sendmail,
+            recipients=customer_list,
+            sender= self.sender_email[0].email_id,
+            subject=self.subject,
+            message=self.template_html,
+            send_after=self.schedule_datetime,
+        )
+
+    def on_submit(self):
+        self.mail_send()
+
+    
