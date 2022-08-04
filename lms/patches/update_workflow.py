95efef553d5f9b7f6eedb584cce21329888642b7
Aditi <aditi.patade@atriina.com>
2022-08-04 09:17:02 +0000

    Feat : Cancelled auto rejection changes

--- lms/patches/update_workflow.py
+++ lms/patches/update_workflow.py
@@ -3,7 +3,13 @@ import frappe
 
 def execute():
     frappe.reload_doc("Lms", "DocType", "Workflow")
+
+    frappe.db.sql("TRUNCATE `tabWorkflow`")
+    frappe.db.sql("TRUNCATE `tabWorkflow Document State`")
+    frappe.db.sql("TRUNCATE `tabWorkflow Transition`")
+    frappe.db.commit()
+
     path = frappe.get_app_path("lms", "patches", "imports", "workflow1.csv")
     frappe.core.doctype.data_import.data_import.import_file(
-        "Workflow", path, "Update", console=True
+        "Workflow", path, "Insert", console=True
     )
