262b3172b4b1be56ec32960bca02cb5e1f6c8b54
Aditi <aditi.patade@atriina.com>
2022-08-04 11:08:18 +0000

    Feat : Auto approval and rejection

--- lms/lms/doctype/loan_transaction/loan_transaction.py
+++ lms/lms/doctype/loan_transaction/loan_transaction.py
@@ -656,8 +656,6 @@ class LoanTransaction(Document):
     def before_save(self):
         loan = self.get_loan()
         self.lender = loan.lender
-        self.instrument_type = loan.instrument_type
-        self.scheme_type = loan.scheme_type
         if (
             self.transaction_type == "Withdrawal"
             and self.allowable > self.requested
