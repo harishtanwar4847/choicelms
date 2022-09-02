65065bc9abfc12a006b4116567461e1b622565da
akash dagade <akash.dagade@atriina.com>
2022-09-02 06:06:50 +0000

    Feat : only  print

--- lms/lms/doctype/loan_application/loan_application.py
+++ lms/lms/doctype/loan_application/loan_application.py
@@ -450,7 +450,7 @@ class LoanApplication(Document):
         )
 
     def on_update(self):
-        print("print")
+        print("Prod")
         if self.status == "Approved":
             if not self.loan:
                 values21 = {"value": (self.status, "inside if on update line368")}
