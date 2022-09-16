a45045a40334e2971a440292f13972a3550c736f
akash dagade <akash.dagade@atriina.com>
2022-09-16 09:50:20 +0000

    Feat : reason code changed from 05 to 06

--- lms/lms/doctype/loan_application/loan_application.py
+++ lms/lms/doctype/loan_application/loan_application.py
@@ -1378,7 +1378,7 @@ class LoanApplication(Document):
                 "PledgeeBOID": self.pledgee_boid,
                 "PRFNumber": prf_number,
                 "ExpiryDate": self.expiry_date.strftime("%d%m%Y"),
-                "ReasonCode": "05",
+                "ReasonCode": "06",
                 "ISINDTLS": securities_array,
             }
             headers = las_settings.cdsl_headers()
