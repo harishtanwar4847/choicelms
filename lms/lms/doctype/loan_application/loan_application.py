0629125485b649d3c5f2f5b462c284f753a9e167
Omkar Darves <omkar.darves@atriina.com>
2022-11-02 11:19:45 +0000

    sms testing dynamic link change

--- lms/lms/doctype/loan_application/loan_application.py
+++ lms/lms/doctype/loan_application/loan_application.py
@@ -1792,11 +1792,15 @@ class LoanApplication(Document):
                     "Increase loan application rejected",
                 )
                 if self.loan and not self.loan_margin_shortfall
+                # else (
+                #     "Dear Customer,\nSorry! Your loan application was turned down since the {} was not successful due to technical reasons. We regret the inconvenience caused. Please try again after sometime or reach out to us through 'Contact Us' on the app  -Spark Loans".format(
+                #         msg_type
+                #     ),
+                #     "Pledge rejected",
+                # )
                 else (
-                    "Dear Customer,\nSorry! Your loan application was turned down since the {} was not successful due to technical reasons. We regret the inconvenience caused. Please try again after sometime or reach out to us through 'Contact Us' on the app  -Spark Loans".format(
-                        msg_type
-                    ),
-                    "Pledge rejected",
+                    """Dear Customer,
+Sorry! Your loan application was turned down since the lien was not successful due to technical reasons. We regret the inconvenience caused. Please try again after a while, or reach out via the 'Contact Us' section of the app- https://bit.ly/3gln78X -Spark Loans"""
                 )
             )
             fcm_notification = frappe.get_doc(
