a667910308de0042b61f44060973f613b0fb372b
Omkar Darves <omkar.darves@atriina.com>
2023-02-22 13:51:12 +0000

    Update __init__.py

--- lms/__init__.py
+++ lms/__init__.py
@@ -54,7 +54,7 @@ from .exceptions import *
 
 # from lms.exceptions.UserNotFoundException import UserNotFoundException
 
-__version__ = "5.13.3"
+__version__ = "5.13.4"
 
 user_token_expiry_map = {
     "OTP": 10,
