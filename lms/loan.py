82249add143921d612f0cf472f72bda4230c1f18
Omkar Darves <omkar.darves@atriina.com>
2023-03-01 06:38:04 +0000

    file id null for testing

--- lms/loan.py
+++ lms/loan.py
@@ -220,7 +220,7 @@ def esign(**kwargs):
 
             return utils.respondWithSuccess(
                 message=_("Esign URL."),
-                data={"esign_url": url, "file_id": None},
+                data={"esign_url": url, "file_id": data.get("id")},
             )
         except requests.RequestException as e:
             raise utils.exceptions.APIException(str(e))
