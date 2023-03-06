371c0fd1c429d024f7cb8058c04fd6eac71717a4
Omkar Darves <omkar.darves@atriina.com>
2023-03-06 12:10:54 +0000

    SPRK 176 - Code and Cron optimization - Process Pledge cron(CDSL pledge setup)

--- lms/lms/doctype/loan_application/loan_application_list.js
+++ lms/lms/doctype/loan_application/loan_application_list.js
@@ -2,5 +2,18 @@
 // For license information, please see license.txt
 
 frappe.listview_settings["Loan Application"] = {
+  onload: function (listview) {
+    frappe.db.get_single_value("LAS Settings", "debug_mode").then((res) => {
+      if (res) {
+        listview.page.add_inner_button(__("Process Pledge"), function () {
+          frappe.call({
+            method:
+              "lms.lms.doctype.loan_application.loan_application.process_pledge",
+            freeze: true,
+          });
+        });
+      }
+    });
+  },
   hide_name_column: true, // hide the last column which shows the `name`
 };
