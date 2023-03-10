9d91642dfd48b59bbb4aa85f8368b8c50c905f28
akash dagade <akash.dagade@atriina.com>
2023-03-10 10:28:27 +0000

    Feat : user variable handling in offline file upload function

--- lms/__init__.py
+++ lms/__init__.py
@@ -2613,8 +2613,8 @@ def customer_file_upload(upload_file):
                         },
                     )
                     if res_email or res_mobile:
-                        user = frappe.get_doc("User", d_name)
                         d_name = res_email[0].name if res_email else res_mobile[0].name
+                        user = frappe.get_doc("User", d_name)
                         offline_customer.user_status = "Failure"
                         offline_customer.user_remarks = "Duplicate Value"
                         offline_customer.customer_status = "Failure"
