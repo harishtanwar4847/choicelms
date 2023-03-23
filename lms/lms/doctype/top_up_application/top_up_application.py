7000d75d31977a01c54330b44af86424b397575d
akash dagade <akash.dagade@atriina.com>
2023-03-23 14:06:37 +0000

    Feat : error message hanling for ltv change

--- lms/lms/doctype/top_up_application/top_up_application.py
+++ lms/lms/doctype/top_up_application/top_up_application.py
@@ -671,7 +671,7 @@ class TopupApplication(Document):
             self.customer_name = loan.customer_name
             if not updated_top_up_amt or updated_top_up_amt < self.top_up_amount:
                 frappe.throw("Top up not available")
-            if self.top_up_amount <= 0:
+            if self.top_up_amount <= 0 and not self.status == "Rejected":
                 frappe.throw("Top up can not be approved with Amount Rs. 0")
         if self.status == "Approved":
             current = frappe.utils.now_datetime()
