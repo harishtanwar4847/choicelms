74f455dc625aac2c26f499a02ecbb1fd489ef94c
vagrant <vagrant@vagrant.vm>
2023-04-06 08:42:05 +0000

    Feat : patch change

--- lms/patches/custom_roi_patch.py
+++ lms/patches/custom_roi_patch.py
@@ -7,10 +7,19 @@ def execute():
     for loan in frappe.get_all("Loan", pluck="name"):
         try:
             loan = frappe.get_doc("Loan", loan)
-            loan.is_default = 1
-            loan.wef_date = frappe.utils.now_datetime().date()
-            loan.save()
-            frappe.db.commit()
+            frappe.db.set_value(
+                "Loan",
+                loan.name,
+                {
+                    "is_default": 1,
+                    "base_interest": 1.24,
+                    "rebate_interest": 0.2,
+                    "custom_base_interest": 1.24,
+                    "custom_rebate_interest": 0.2,
+                    "wef_date": frappe.utils.now_datetime().date(),
+                },
+                update_modified=False,
+            )
         except:
             frappe.log_error(
                 title="custom roi patch",
