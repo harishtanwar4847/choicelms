dfaf1e5c34cee5a2da6488c062979ed2cd7d2eba
akash dagade <akash.dagade@atriina.com>
2023-04-17 11:39:27 +0000

    revoke token fix

--- lms/lms/doctype/unpledge_application/unpledge_application.py
+++ lms/lms/doctype/unpledge_application/unpledge_application.py
@@ -750,7 +750,7 @@ def initiate_revoc(unpledge_application_name):
                         data = {
                             "revocinitiate": {
                                 "reqrefno": i.revoc_ref_no,
-                                "revoctoken": i.revoctoken,
+                                "revoctoken": i.revoc_token,
                                 "lienrefno": i.prf,
                                 "pan": user_kyc.pan_no,
                                 "regemailid": customer.mycams_email_id,
