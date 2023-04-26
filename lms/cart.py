d79040651b0acf8be83a8a699587a471247a45a6
akash dagade <akash.dagade@atriina.com>
2023-04-26 11:58:42 +0000

    Feat :get tnc fix

--- lms/cart.py
+++ lms/cart.py
@@ -1030,14 +1030,10 @@ def get_tnc(**kwargs):
         interest_charges_in_amount = float(
             cart.increased_sanctioned_limit
             if cart.loan and not cart.loan_margin_shortfall
-            else cart.eligible_loan
+            else eligibile_loan
         ) * (roi_ / 100)
         interest_per_month = float(interest_charges_in_amount / 12)
-        final_payment = float(interest_per_month) + (
-            cart.increased_sanctioned_limit
-            if cart.loan and not cart.loan_margin_shortfall
-            else cart.eligible_loan
-        )
+        final_payment = float(interest_per_month) + (eligibile_loan)
         # apr = lms.calculate_apr(
         #     data.get("cart_name"),
         #     roi_,
