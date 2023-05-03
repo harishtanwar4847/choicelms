72eb2fc75ff456c7e4e0676863c31647bc79dcbc
akash dagade <akash.dagade@atriina.com>
2023-05-03 12:24:22 +0000

    Feat : pages extraction for sanction letter

--- lms/lms/doctype/spark_loan_renewal_application/spark_loan_renewal_application.py
+++ lms/lms/doctype/spark_loan_renewal_application/spark_loan_renewal_application.py
@@ -1152,15 +1152,14 @@ Congratulations! Your loan renewal process is completed. Please visit the spark.
                     file_base_name = pdf_file_path.replace(".pdf", "")
                     reader = PdfReader(pdf_file_path)
                     pages = [
+                        24,
+                        25,
+                        26,
+                        27,
+                        28,
+                        29,
                         30,
                         31,
-                        32,
-                        33,
-                        34,
-                        35,
-                        36,
-                        37,
-                        38,
                     ]  # page 1, 3, 5
                     pdfWriter = PdfWriter()
                     for page_num in pages:
--- lms/lms/doctype/top_up_application/top_up_application.py
+++ lms/lms/doctype/top_up_application/top_up_application.py
@@ -1072,15 +1072,14 @@ class TopupApplication(Document):
                 file_base_name = pdf_file_path.replace(".pdf", "")
                 pdf = PdfReader(pdf_file_path)
                 pages = [
+                    24,
+                    25,
+                    26,
+                    27,
+                    28,
+                    29,
                     30,
                     31,
-                    32,
-                    33,
-                    34,
-                    35,
-                    36,
-                    37,
-                    38,
                 ]  # page 1, 3, 5
                 pdfWriter = PdfWriter()
                 for page_num in pages:
